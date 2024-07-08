71336f35388ae5e8c4a736cbe66caa3129fc8750
moshepiltz <p052710200@gmail.com>
2024-07-08 23:50:29 +0300

    Merge branch 'main' of https://github.com/talyaron/Fullstack-Jun2024

---
+++ 03-CSS/01-css-selectors-02-07-2024/homePuzzle/moshe piltz/index.vb
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head><link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EXPERIENCE
+     </title>
+</head>
+</body>
+
+<div class="frame red-frame">
+    <P>מבצע מוגבל בלבנון זה לא אפשרי, חשבו על אופציה נוספת<br>יש פיתרון?</P>
+   </div>
+  
+
+<div class="frame blue-frame">
+    <P>מבצע מוגבל בלבנון זה לא אפשרי, חשבו על אופציה נוספת<br>יש פיתרון?</P>
+</div>
+    <div class="frame green-frame">
+        <P>מבצע מוגבל בלבנון זה לא אפשרי, חשבו על אופציה נוספת<br>יש פיתרון?</P>
+       </div>
+   
+
+        <div class="outerframe">
+    <iframe class="shape color-red" frameborder="0"></iframe>
+    <iframe class="shape color-blue" frameborder="0"></iframe>
+    <iframe class="shape color-green" frameborder="0"></iframe>
+    <iframe class="shape color-black" frameborder="0"></iframe></div>
+
+        
+        
+
+        
+    </body>
+
+</html>
