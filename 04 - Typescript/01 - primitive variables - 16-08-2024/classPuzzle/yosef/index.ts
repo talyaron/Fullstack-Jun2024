42b98ee1e8102c7298b9dfc276840a4fcb33bd84
Yosef Ibrahim <yosefib88@gmail.com>
2024-08-16 12:48:40 +0300

    work
    שלי

--- 04 - Typescript/01 - primitive variables - 16-08-2024/classPuzzle/yosef/index.ts
+++ 04 - Typescript/01 - primitive variables - 16-08-2024/classPuzzle/yosef/index.ts
@@ -1,12 +1,15 @@
-let FirstName:string | null="yosef";
-let LastName:sting | null="ibrahimi";
-let Email:string | null="yosefib88@gmail.com";
-let Phone:number:0545732050;
+// let FirstName:string | null="yosef";
+// let LastName:string | null="ibrahimi";
+// let Email:string | null="yosefib88@gmail.com";
+// let Phone:string | null="0545732050";
 
-FirstName = prompt("Please enter your first name");
-LastName = prompt("Please enter your last name");
-Email = prompt("Please enter your email");
-Phone = Number(prompt("Please enter your phone number"));
+// FirstName = prompt("Please enter your first name");
+// LastName = prompt("Please enter your last name");
+// Email = prompt("Please enter your email");
+// Phone = (prompt("Please enter your phone number"));
 
-document.write(`Hello ${FirstName} ${LastName}! Your email is ${Email} and your phone number is ${Phone}.`);
+// document.write(`Hello ${FirstName} ${LastName}! Your email is ${Email} and your phone number is ${Phone}.`);
 
+let color:string | null=prompt("Please enter a color");
+
+document.body.style.backgroundColor = color || "red";
---
+++ 04 - Typescript/02 - functions - 16-08-2024/homePuzzles/yosef/level1/index.ts
@@ -0,0 +1,3 @@
+let FullName: string | null=prompt("Please enter your full name");
+let Color: string | null=prompt("please enter your color");
+
