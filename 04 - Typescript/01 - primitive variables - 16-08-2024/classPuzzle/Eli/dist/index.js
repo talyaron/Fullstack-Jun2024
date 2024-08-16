f2ec4ce588f11535dd77b122632e05e64f66a8e6
Elizigi <elizigi876@gmail.com>
2024-08-16 11:04:04 +0300

    TaskFinished

---
+++ 04 - Typescript/01 - primitive variables - 16-08-2024/classPuzzle/Eli/dist/index.js
@@ -0,0 +1,9 @@
+var first_name;
+var last_name;
+var phon_num;
+var email;
+first_name = prompt("please enter first name");
+last_name = prompt("please enter last name");
+phon_num = Number(prompt("please enter first name"));
+email = prompt("please enter your Email adress");
+document.write("hello, my name is " + first_name + " " + last_name + " . You can call me at " + phon_num + " or email me at " + email);
