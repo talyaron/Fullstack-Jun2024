c440b44f9b15beca0a267e4476b264fd3b7a611e
Yulia <u.kaganovich@gmail.com>
2024-08-16 12:41:07 +0300

    puzzle

---
+++ 04 - Typescript/01 - primitive variables - 16-08-2024/classPuzzle/Yulia/index.ts
@@ -0,0 +1,14 @@
+let first_name: string | null 
+let last_name: string | null
+let phone_number: string|null
+let email: string | null
+
+first_name = "Yulia"
+last_name = "Kaganovich"
+phone_number = "0503519333"
+email = "u.kaganovich@gmail.com"
+
+document.write(`hello, my name is ${first_name} ${last_name}. You can call me at ${phone_number} or email me at ${email}.`)
+
+
+
