7115c391d1b34c35a50a0119244f703103bc94f7
raamhumphries <raamhomfris2006@gmail.com>
2024-08-20 15:41:32 +0300

    class exresise from 16.8.24

---
+++ 04 - Typescript/01 - primitive variables - 16-08-2024/classPuzzle/raam/typescripte.ts
@@ -0,0 +1,8 @@
+let firstname :string|null ;
+let lastname :string|null;
+let phonenumber :number |null;
+let email :string|null;
+firstname = prompt("Please enter your name");
+lastname = prompt("Please enter your last name");
+phonenumber = Number(prompt("Please enter your phone number"));
+email = prompt("Please enter your email");
