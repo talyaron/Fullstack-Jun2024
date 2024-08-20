230da3add4f02884d15ece56d85709545320b2c9
raamhumphries <raamhomfris2006@gmail.com>
2024-08-20 20:10:47 +0300

    starting home pazzle

---
+++ 04 - Typescript/03 - conditions/HomePuzzles/raamhumphries/dist/typescript.js
@@ -0,0 +1,4 @@
+// ### Exercise 4: Leap Year Checker
+// Write a program that asks the user to enter a year, then tells them if it's a leap year or not.
+//  A leap year is divisible by 4, except for century years (years ending with 00). 
+// The century years are leap years only if they are perfectly divisible by 400.
