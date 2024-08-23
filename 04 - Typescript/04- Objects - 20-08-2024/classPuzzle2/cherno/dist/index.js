3aac364d71f8eb095c601697797b698b57d90dbe
Cherno <daniel.cherno9@gmail.com>
2024-08-23 10:49:30 +0300

    added classpuzzle

---
+++ 04 - Typescript/04- Objects - 20-08-2024/classPuzzle2/cherno/dist/index.js
@@ -0,0 +1,23 @@
+// render objects to DOM
+//make it a function that get the user details
+function enterDetails() {
+    var userName = prompt("pls enter your name");
+    var userCity = prompt("pls enter your city");
+    debugger;
+    try {
+        if (null == userCity ||
+            null == userName ||
+            "" == userCity ||
+            "" == userName) {
+            throw new Error("no input");
+        }
+        return { name: String(userName), city: String(userCity) };
+    }
+    catch (e) {
+        console.error(e);
+        return { name: String(userName), city: String(userCity), error: e };
+    }
+}
+// get the function output and render to the DOM (document.write)
+var user = enterDetails();
+document.write("Hi " + user.name + "! You live in " + user.city + ".");
