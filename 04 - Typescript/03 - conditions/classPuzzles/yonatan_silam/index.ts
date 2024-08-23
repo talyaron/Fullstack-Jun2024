8faa5e3599ef74cc3dc126643cda26ed74ce02e3
Tal <tal.yaron@gmail.com>
2024-08-23 09:07:15 +0300

    cont

--- 04 - Typescript/03 - conditions/classPuzzles/yonatan_silam/index.ts
+++ 04 - Typescript/03 - conditions/classPuzzles/yonatan_silam/index.ts
@@ -1,29 +1,36 @@
-const score: number= Number(prompt("pls enter a score."));
+const score: number = Number(prompt("pls enter a score."));
 // debugger;
-var endScore:string= "";
+var endScore: string = "";
 checkScore(score);
-function checkScore(number : number):void{
-    if(number >=0 && number<=100){
-        endScore=giveScore(number);
+
+
+function checkScore(number: number): void {
+    if (number >= 0 && number <= 200) {
+        endScore = giveScore(number);
         document.write(endScore);
-    }else{
-        const score: number= Number(prompt("pls enter a score between 0-100."));
+    } else {
+        const score: number = Number(prompt("pls enter a score between 0-100."));
         checkScore(score);
 
-}
+    }
 }
 
-function giveScore(number:number):string{
-        if(number<= 59)
+function giveScore(number: number): string {
+    try {
+        if(number <0 || number > 100) throw new Error("Score should be between 0-100");
+
+        if (number <= 59)
             return "F"
-        else if(number<= 69)
+        else if (number <= 69)
             return "d"
-        else if(number<= 79)
+        else if (number <= 79)
             return "c"
-        else if(number<= 89)
+        else if (number <= 89)
             return "b"
-         else if(number<= 100)
-            return "A"
-        else
-        return "";
+        else (number <= 100)
+        return "A"
+    } catch (e) {
+        console.error(e);
+        return "error";
+    }
 }
