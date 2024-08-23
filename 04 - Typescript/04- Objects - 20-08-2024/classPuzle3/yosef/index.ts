db7363b37b690d06a2b1c779558a327c451aee6a
Yosef Ibrahim <yosefib88@gmail.com>
2024-08-23 19:01:54 +0300

    123

---
+++ 04 - Typescript/04- Objects - 20-08-2024/classPuzle3/yosef/index.ts
@@ -0,0 +1,39 @@
+// interface rectangle {
+//     width: number;
+//     height: number;
+// } 
+
+
+
+// const new123: rectangle = {
+//     width : prompt("enter width");
+//     height : prompt("enter height");
+// }
+
+// function get_surface (newnew: rectangle): number{
+//     return newnew.width * newnew.height;
+// }
+
+// document.write('the surface is : ' get_surface(new123));
+
+
+
+interface book{
+    title: string;
+    maker: string;
+    year: number;
+}
+
+function get_book(new_book: book): string {
+    const new_book : new_book = {
+        title: prompt("enter title"),
+        maker: prompt("enter maker"),
+        year: Number(prompt("enter year"))
+    }
+}
+
+
+
+
+
+
