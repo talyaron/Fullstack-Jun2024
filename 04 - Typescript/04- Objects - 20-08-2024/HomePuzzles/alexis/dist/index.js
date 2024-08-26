0e2a927c79f14277ce2e9aa3d09d2fd568eecbe2
alexis <gog20@icloud.com>
2024-08-26 16:53:54 +0300

    3

--- 04 - Typescript/04- Objects - 20-08-2024/HomePuzzles/alexis/dist/index.js
+++ 04 - Typescript/04- Objects - 20-08-2024/HomePuzzles/alexis/dist/index.js
@@ -1,55 +1,8 @@
 // // ### 1. Book Information
 // // Create an interface for a Book with properties: title, author, and publicationYear.
 // // Write a function that takes a Book object and returns a formatted string with the book's information.
-function getValue() {
-    try {
-        var value = Number(prompt("enter a temperature value"));
-        var unit = prompt("enter a temperature value");
-        var temp = {
-            value: value,
-            unit: unit
-        };
-        if (isNaN(value) || (unit !== "c" || unit !== "C" || unit !== "f" || unit !== "F")) {
-            throw new Error("Invalid input.");
-        }
-        else
-            return temp;
-    }
-    catch (error) {
-        console.error(error);
-        return {
-            value: 0,
-            unit: ""
-        };
-    }
+function getData() {
+    var name = prompt("What's your name?");
+    var birthDate = Number(prompt("When is your birthday?"));
+    var referenceDate = Number(prompt("What day is it today?"));
 }
-var result = getValue();
-function convertTemp(result) {
-    try {
-        if (result.unit === "c" || result.unit === "C") {
-            var value = (result.value - 32) * 5 / 9;
-            var temperature_1 = {
-                value: value,
-                unit: result.unit
-            };
-            console.log(temperature_1);
-        }
-        else if (result.unit === "f" || result.unit === "F") {
-            var value = (result.value * 9 / 5) + 32;
-            console.log(temperature);
-        }
-        var temperature = {
-            value: value,
-            unit: result.unit
-        };
-        return temperature;
-    }
-    catch (error) {
-        console.error(error);
-        return {
-            value: 0,
-            unit: ""
-        };
-    }
-}
-convertTemp(result);
--- 04 - Typescript/04- Objects - 20-08-2024/HomePuzzles/alexis/index.ts
+++ 04 - Typescript/04- Objects - 20-08-2024/HomePuzzles/alexis/index.ts
@@ -76,70 +76,99 @@
 // const circle: Circle = getRadius();
 // displayCircleInfo(circle);
 
-// ### 3. Temperature Converter
-// Create an interface for Temperature with properties: value and unit (either 'C' or 'F').
-// Write a function that takes a Temperature object and converts it to the opposite unit, returning a new Temperature object.
+// // ### 3. Temperature Converter
+// // Create an interface for Temperature with properties: value and unit (either 'C' or 'F').
+// // Write a function that takes a Temperature object and converts it to the opposite unit, returning a new Temperature object.
 
-interface Temperature {
-  value: number;
-  unit: string;
+// interface Temperature {
+//   value: number;
+//   unit: string;
+// }
+
+// function getValue(): Temperature {
+//   try {
+//     const value: number = Number(prompt("enter a temperature value"));
+//     const unit: string = prompt("enter a temperature value");
+
+//     const temp: Temperature = {
+//       value: value,
+//       unit: unit,
+//     };
+//     if (
+//       isNaN(value) ||
+//       (unit !== "c" && unit !== "C" && unit !== "f" && unit !== "F")
+//     ) {
+//       throw new Error("Invalid input.");
+//     } else console.log(temp);
+//     return temp;
+//   } catch (error) {
+//     console.error(error);
+//     return {
+//       value: 0,
+//       unit: "",
+//     };
+//   }
+// }
+
+// const result: Temperature = getValue();
+
+// function convertTemp(result: Temperature) {
+//   try {
+//     if (result.unit === "c" || result.unit === "C") {
+//       const value =  (result.value * 9) / 5 + 32;
+//       const temperature: Temperature = {
+//         value: value,
+//         unit: "f",
+//       };
+//       console.log(temperature);
+//       return temperature;
+//     } else if (result.unit === "f" || result.unit === "F") {
+//       const value =((result.value - 32) * 5) / 9;
+//       const temperature: Temperature = {
+//         value: value,
+//         unit: "c",
+//       };
+//       console.log(temperature);
+//       return temperature;
+//     }
+//   } catch (error) {
+//     console.error(error);
+//     return {
+//       value: 0,
+//       unit: "",
+//     };
+//   }
+// }
+// const final = convertTemp(result);
+
+// function display(final:Temperature){
+//     document.write(`The temperature is ${final.value} ${final.unit}`);
+
+//     console.log(`The temperature is ${final.value} ${final.unit}`);}
+
+// display(final);
+
+
+// ### 4. Person Age Calculator
+// Define an interface for a Person with name and birthDate properties. 
+// Implement a function that takes a Person object and a reference date, then returns the person's age at that reference date.
+
+
+interface Person{
+    name:string;
+    birthDate:number;
 }
 
-function getValue(): Temperature {
-  try {
-    let value:number = Number(prompt("enter a temperature value"));
-    let unit:string = prompt("enter a temperature value");
-  
-
-    const temp: Temperature = {
-      value: value,
-      unit: unit,
-    };
-    if (isNaN(value)||(unit !== "c" || unit !== "C" || unit !== "f" || unit !== "F")) {
-        throw new Error("Invalid input.");
-      }
-  else
-    return temp;
-  } catch (error) {
-    console.error(error);
-    return {
-      value: 0,
-      unit: "",
-    };
-  }
+function getData(){
+    const name:string = prompt("What's your name?");
+    const birthDate:number = Number(prompt("When is your birthday?"));
+    const referenceDate:number = Number(prompt("What day is it today?"));
+    
 }
 
-const result:Temperature = getValue();
 
-function convertTemp(result:Temperature):Temperature{
-    try {
 
-        if (result.unit === "c"||result.unit==="C"){
-           const value = (result.value - 32) * 5/9;
-        const temperature:Temperature={
-            value:value,
-            unit:result.unit,
-        }
-           console.log(temperature);
-        }else 
-        if (result.unit === "f"||result.unit==="F"){
-            const value = (result.value *9/5) + 32;
-        
-            console.log(temperature);
-         }
-         const temperature:Temperature={
-            value:value,
-            unit:result.unit,
-        }
-         return temperature;
 
-    } catch (error) {
-        console.error(error);
-        return{
-            value:0,
-            unit:"",
-        }
-    }
-}
 
-convertTemp(result);
+
+
