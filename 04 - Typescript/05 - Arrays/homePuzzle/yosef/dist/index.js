d2586825b8b0708df7559023d94a3fbc91577e91
Yosef Ibrahim <yosefib88@gmail.com>
2024-08-27 21:00:19 +0300

    123

---
+++ 04 - Typescript/05 - Arrays/homePuzzle/yosef/dist/index.js
@@ -0,0 +1,19 @@
+var books = [
+    {
+        title: 'book1',
+        auther: 'yose232323f',
+        year: 1992
+    },
+    {
+        title: 'book2',
+        auther: 'yose232323f',
+        year: 1890
+    },
+    {
+        title: 'book3',
+        auther: 'yose232323f',
+        year: 1999
+    }
+];
+var new_arry = books.push();
+console.log(books);
---
+++ 04 - Typescript/05 - Arrays/homePuzzle/yosef/index.ts
@@ -0,0 +1,33 @@
+interface book {
+    title : String;
+    auther : String;
+    year : Number;
+}
+
+const books: book[] = [
+    {
+        title : 'book1',
+        auther : 'yose232323f',
+        year : 1992
+
+    }
+
+    {
+        title : 'book2',
+        auther : 'yose232323f',
+        year : 1890
+
+    }
+
+    {
+        title : 'book3',
+        auther : 'yose232323f',
+        year : 1999
+
+    }
+    
+]
+
+const new_arry = books.push() 
+console.log(books);
+
