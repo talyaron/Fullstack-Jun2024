3a46c18fd15b6d9ac3110141c7d805c07e6497c2
Alexis Vishnevezky <gog20@icloud.com>
2024-08-28 12:08:04 +0300

    homepuzzle av

--- 04 - Typescript/04- Objects - 20-08-2024/HomePuzzles/alexis/dist/index.js
+++ 04 - Typescript/04- Objects - 20-08-2024/HomePuzzles/alexis/dist/index.js
@@ -3,49 +3,56 @@
 // // Write a function that takes a Book object and returns a formatted string with the book's information.
 function getData() {
     try {
-        var name = prompt("What's your name?");
-        var birthDateInput = prompt("When is your birthday? (YYYY-MM-DD)");
-        if (!name || !birthDateInput) {
-            throw new Error("Name or birthdate is missing.");
-        }
-        var birthDate = new Date(birthDateInput);
-        var person_1 = {
+        var name = prompt("enter a name");
+        var price = Number(prompt("enter a price"));
+        var category = prompt("enter a category");
+        var product_1 = {
             name: name,
-            birthDate: birthDate
+            price: price,
+            category: category
         };
-        console.log(person_1);
-        return person_1;
+        console.log(product_1);
+        return product_1;
     }
     catch (error) {
-        console.error("An error has occurred:", error.message);
+        console.error(error);
         return {
             name: "",
-            birthDate: new Date(0)
+            price: 0,
+            category: ""
         };
     }
 }
-var person = getData();
-function calculateAge(person) {
+function getDataDiscount() {
     try {
-        var dateInput = prompt("Enter a date (YYYY-MM-DD)");
-        if (!dateInput) {
-            throw new Error("Date is missing.");
-        }
-        var date = new Date(dateInput);
-        var birthDate = person.birthDate;
-        var age = date.getFullYear() - birthDate.getFullYear();
-        var monthDifference = date.getMonth() - birthDate.getMonth();
-        if (monthDifference < 0 ||
-            (monthDifference === 0 && date.getDate() < birthDate.getDate())) {
-            age--;
-        }
-        console.log("Hi, " + person.name + ". Your age today is " + age);
-        document.write("Hi, " + person.name + ", your age today is " + age);
-        return age;
+        var percentage = Number(prompt("enter a discount"));
+        var category = prompt("enter a category");
+        var discount_1 = {
+            category: category,
+            percentage: percentage
+        };
+        console.log(discount_1);
+        return discount_1;
     }
     catch (error) {
-        console.error("An error has occurred during age calculation:", error.message);
-        return 0;
+        console.error(error);
+        return {
+            category: "",
+            percentage: 0
+        };
     }
 }
-calculateAge(person);
+var product = getData();
+var discount = getDataDiscount();
+function discountCalculation(product, discount) {
+    var newPrice;
+    if (product.category === discount.category) {
+        newPrice = (product.price * (100 - discount.percentage)) / 100;
+        console.log(newPrice);
+    }
+    else {
+        console.log("there is now discount");
+    }
+    return newPrice;
+}
+discountCalculation(product, discount);
