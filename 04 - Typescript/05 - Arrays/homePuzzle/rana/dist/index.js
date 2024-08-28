5cc73fd6b73dce01c2ea8cbd8b9cdb79109ced68
Ranazidan1 <halabyrana8@gmail.com>
2024-08-28 20:49:32 +0300

    rana 1

--- 04 - Typescript/05 - Arrays/homePuzzle/rana/dist/index.js
+++ 04 - Typescript/05 - Arrays/homePuzzle/rana/dist/index.js
@@ -1,4 +1,4 @@
-//2. Create an array of `Book` objects to store your book collection-
+// 2. Create an array of `Book` objects to store your book collection-
 var bookCollection = [
     {
         title: "Good Omens",
@@ -16,18 +16,45 @@ var bookCollection = [
         year: 2011
     },
 ];
-//3. Implement the following functions:
-//a. Add a new book to the collection-
-function addBook(newBook) {
-    bookCollection.push(newBook);
-    console.log("Added: " + newBook.title + " by " + newBook.author + " (" + newBook.year + ")");
-    return bookCollection;
-}
-var newBook = {
+// 3. Implement the following functions:
+// a. Add a new book to the collection-
+bookCollection.push({
     title: "The Road",
     author: "Cormac McCarthy",
     year: 2006
-};
-var updatedCollection = addBook(newBook);
-console.log(updatedCollection);
-//b. Display all books in the collection-
+});
+console.log(bookCollection);
+// b. Display all books in the collection-
+function displayAllBooks() {
+    bookCollection.forEach(function (book) { return console.log(book.title + " written by " + book.author + " in(" + book.year + ")"); });
+    return bookCollection;
+}
+displayAllBooks();
+// c. Find a book by its title-
+function findBookByTitle(title) {
+    return bookCollection.find(function (book) { return book.title.toLowerCase() === title.toLowerCase(); });
+}
+;
+var book = findBookByTitle("The Hitchhiker's Guide to the Galaxy");
+if (book) {
+    console.log("Found book: \"" + book.title + "\" by " + book.author);
+}
+else {
+    console.log("Book not found");
+}
+// 4. Use an array method to filter books published after a certain year-
+function filterBooksByYear(year) {
+    return bookCollection.filter(function (book) { return book.year > year; });
+}
+var booksAfter2000 = filterBooksByYear(2000);
+console.log("Books published after 2000:", booksAfter2000);
+var booksAfter1990 = filterBooksByYear(1990);
+console.log("Books published after 1990:", booksAfter1990);
+// 5. Create a function that returns the newest book in the collection-
+function getNewestBook(bookCollection) {
+    return bookCollection.sort(function (a, b) { return b.year - a.year; })[0];
+}
+// 6. Practice using a forEach to display each book's information-
+bookCollection.forEach(function (book) {
+    console.log("The book \"" + book.title + "\" was written by " + book.author + " in " + book.year + ".");
+});
--- 04 - Typescript/05 - Arrays/homePuzzle/rana/index.ts
+++ 04 - Typescript/05 - Arrays/homePuzzle/rana/index.ts
@@ -1,13 +1,14 @@
 //## Exercise 1-
-//1. Create a `Book` object type with these properties-
+// 1. Create a `Book` object type with these properties-
 interface Book {
   title: string;
   author: string;
   year: number;
 }
-//2. Create an array of `Book` objects to store your book collection-
-const bookCollection: Book[] = [ 
-    {
+
+// 2. Create an array of `Book` objects to store your book collection-
+const bookCollection:Book[] = [
+  {
     title: "Good Omens",
     author: "Neil Gaiman",
     year: 1990,
@@ -21,27 +22,54 @@ const bookCollection: Book[] = [
     title: "Bossypants",
     author: "Tina Fey",
     year: 2011,
-  },];
+  },
+];
 
-//3. Implement the following functions:
-//a. Add a new book to the collection-
-function addBook(newBook: Book): Book[] {
-    bookCollection.push(newBook);
-    console.log(`Added: ${newBook.title} by ${newBook.author} (${newBook.year})`);
-    return bookCollection;
+// 3. Implement the following functions:
+
+// a. Add a new book to the collection-
+
+bookCollection.push({
+  title: "The Road",
+  author: "Cormac McCarthy",
+  year: 2006,
+});
+console.log(bookCollection);
+
+// b. Display all books in the collection-
+function displayAllBooks(): Book[] {
+  bookCollection.forEach((book) =>console.log(`${book.title} written by ${book.author} in(${book.year})`));
+  return bookCollection;
 }
+displayAllBooks();
+// c. Find a book by its title-
+function findBookByTitle(title: string): Book | undefined {
+    return bookCollection.find(book => book.title.toLowerCase() === title.toLowerCase());
+  };
+  const book = findBookByTitle("The Hitchhiker's Guide to the Galaxy");
+  if (book) {
+    console.log(`Found book: "${book.title}" by ${book.author}`);
+  } else {
+    console.log("Book not found");
+  }
 
-const newBook: Book = {
-    title: "The Road",
-    author: "Cormac McCarthy",
-    year: 2006,
-};
+// 4. Use an array method to filter books published after a certain year-
+function filterBooksByYear(year: number): Book[] {
+    return bookCollection.filter(book => book.year > year);
+  }
+  const booksAfter2000 = filterBooksByYear(2000);
+console.log("Books published after 2000:", booksAfter2000);
 
-const updatedCollection = addBook(newBook);
-console.log(updatedCollection);
+const booksAfter1990 = filterBooksByYear(1990);
+console.log("Books published after 1990:", booksAfter1990);
 
-//b. Display all books in the collection-
+// 5. Create a function that returns the newest book in the collection-
+function getNewestBook(bookCollection: Book[]): Book | undefined {
+    return bookCollection.sort((a, b) => b.year - a.year)[0];
+  }  
 
+// 6. Practice using a forEach to display each book's information-
 
-
-
+bookCollection.forEach((book) => {
+    console.log(`The book "${book.title}" was written by ${book.author} in ${book.year}.`);
+  });
