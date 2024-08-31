dd75aef817086ce9b7e6045d1d7c8aab10f888de
Toharkenin <109432113+Toharkenin@users.noreply.github.com>
2024-08-31 13:45:25 +0300

    homepuzzle p2

--- 04 - Typescript/06 - DOM manipulations - 30-08-2024/HomePuzzles/Tohar/dist/index.js
+++ 04 - Typescript/06 - DOM manipulations - 30-08-2024/HomePuzzles/Tohar/dist/index.js
@@ -1,3 +1,5 @@
+;
+;
 var navbar = [
     { name: "Mobile/Handhelds" },
     { name: "Laptops" },
@@ -6,11 +8,23 @@ var navbar = [
     { name: "Networking/Iot/Servers" },
     { name: "Networking/Accessories" },
 ];
+var computersList = [
+    { id: 1, name: "Zenebook", price: 7700, sale: false },
+    { id: 2, name: "Vivobook", price: 8000, sale: false },
+    { id: 3, name: "Chromebook", price: 1000, sale: false },
+    { id: 4, name: "ExpertBook", price: 800, sale: false },
+    { id: 5, name: "Next-Gen Console", price: 10000, sale: true },
+    { id: 6, name: "TUF Gaming VG28UQL1A", price: 10000, sale: true },
+    { id: 7, name: "ASUS BR1104C", price: 900, sale: false },
+    { id: 8, name: "ASUS BR1100F", price: 1100, sale: true },
+    { id: 9, name: "BE24EQK", price: 2500, sale: true },
+    { id: 10, name: "ProArt", price: 750, sale: false },
+];
 function renderNavbar() {
     try {
         var navbarElement_1 = document.querySelector("#navbar");
         if (!navbarElement_1)
-            throw new Error("Could not find store navbar element");
+            throw new Error("Could not find navbar element");
         navbar.forEach((function (item) {
             var menuItem = document.createElement("div");
             menuItem.innerHTML = "\n            <a>" + item.name + "</a>\n            ";
@@ -23,4 +37,24 @@ function renderNavbar() {
     }
 }
 ;
+function renderComputersList() {
+    try {
+        var computersElement_1 = document.querySelector("#computersList");
+        if (!computersElement_1)
+            throw new Error("Could not find computers element!");
+        computersList.forEach((function (computer) {
+            var computerItem = document.createElement("div");
+            if (computer.price <= 1000) {
+                computerItem.innerHTML = "\n            <h1>" + computer.name + "</h1>\n            <h2>$" + computer.price + "</h2>\n            <button>Show More</button>\n            ";
+            }
+            computerItem.classList.add("navbarItem");
+            computersElement_1.appendChild(computerItem);
+            console.log(computerItem);
+        }));
+    }
+    catch (e) {
+        console.error(e);
+    }
+}
 renderNavbar();
+renderComputersList();
