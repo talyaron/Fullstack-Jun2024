8d948d04f212c2e832e252551c68d82033fedfc2
Yosef Ibrahim <yosefib88@gmail.com>
2024-08-31 19:18:17 +0300

    מתקדמים יפה

--- 04 - Typescript/06 - DOM manipulations - 30-08-2024/HomePuzzles/yosef/index.ts
+++ 04 - Typescript/06 - DOM manipulations - 30-08-2024/HomePuzzles/yosef/index.ts
@@ -2,6 +2,14 @@ interface navbar{
     name: string;
 }
 
+interface Computer{
+    id: number;
+    name: string;
+    price: number;
+    sale: boolean;
+    img: string;
+}
+
 const navbarList: navbar[] = [
     {name: 'Mobile/Handheld'},
     {name: 'Laptops'},
@@ -10,6 +18,44 @@ const navbarList: navbar[] = [
     {name: 'Newwork/Lot/Servers'},
     {name: 'Accessories'}
 ]
+
+const Computers : Computer[] = [
+    {id: 1, name: 'ASUS TUF Gaming F15', price: 1200, sale: false, img:'https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg'},
+    {id: 2, name: 'ASUS ROG Strix G535', price: 1500, sale: true, img:'https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg'},
+    {id: 3, name: 'ASUS ZenBook Pro Duo', price: 1000, sale: false, img:'https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg'},
+    {id: 4, name: 'ASUS ZenBook Flip C', price: 1300, sale: true, img:'https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg'}
+    {id: 5, name: 'MSI Laptop Turbo', price: 900, sale: true, img:'https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg'}
+    {id: 6, name: 'Acer Aspire 5', price: 700, sale: false, img:'https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg'}
+    {id: 7, name: 'HP Spectre x360', price: 1100, sale: false, img:'https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg'}
+    {id: 8, name: 'Lenovo Ideapad 5', price: 800, sale: false, img:'https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg'}
+    {id: 9, name: 'Dell 2025', price: 900, sale:true, img:'https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg'}
+    {id: 10, name: 'Lenovo Ideapad 7', price: 1000, sale: false, img:'https://cdn.britannica.com/77/170477-050-1C747EE3/Laptop-computer.jpg'}
+]
+
+function displayComputers() {
+    try{
+        const comp_item = document.querySelector('#computers') as HTMLElement;
+        if(!comp_item) throw new Error('No such element');
+
+        Computers.forEach(new_computer => {
+            const computerItem = document.createElement('div')
+            comp_item.innerHTML = 
+            `
+            <h1>Computer ${new_computer.name}
+            <p>the ID is ${new_computer.id}</p>
+            <h2>Price: $${new_computer.price}</h2>
+            <p>Sale: ${new_computer.sale? 'Yes' : 'No'}</p>
+            <img src="${new_computer.img}" />
+            `
+            comp_item.appendChild(computerItem);
+            computerItem.classList.add('computer-item');
+        })
+    }
+    catch(error){
+        console.error(error);
+    }
+
+}
 
 console.log(navbarList);
 
@@ -20,12 +66,16 @@ function displayNavbar(){
 
          // const x = document.querySelector(`#navbar`) as HTMLElement;
         // x.style.display = "block";
+        const asus_img = document.createElement('img');
+        asus_img.src = 'https://www.freepnglogos.com/uploads/logo-asus-png/asus-logo-hd-photo-15.png'
+        navbar.appendChild(asus_img);
+        navbar.classList.add('img_navbar');
+
 
         navbarList.forEach(item => {
             const navbarItem = document.createElement('div');
             navbarItem.innerHTML = `
-            <a href="#">${item.name}</a>
-            `;
+            <a href="https://www.asus.com/">${item.name}</a>`;
             navbar.appendChild(navbarItem);
             navbarItem.classList.add('nav-item');
 
@@ -37,4 +87,6 @@ function displayNavbar(){
         console.error(error);
     }
 }
+
 displayNavbar();
+displayComputers();
