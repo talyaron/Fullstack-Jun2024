4b3f3e8069893e3da8b9d5e60d47a6475f3c372e
Ahmadzakaria22 <az622892@gmail.com>
2024-09-01 19:07:07 +0300

    arrayHomePuzzel
    Q1+Q2

--- 04 - Typescript/05 - Arrays/classPuzzle/Ahmad-Array/dist/index.js
+++ 04 - Typescript/05 - Arrays/classPuzzle/Ahmad-Array/dist/index.js
@@ -1,3 +1,14 @@
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var names = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Frank', 'Grace', 'Heidi', 'Ivan', 'Judy'];
 var getRandomName = function () { return names[Math.floor(Math.random() * names.length)]; };
 var getRandomAge = function () { return Math.floor(Math.random() * 21) + 20; };
@@ -14,5 +25,5 @@ var olderStudents = students.filter(function (person) { return person.age > 30; 
 console.log(olderStudents);
 var sortedOlderStudents = olderStudents.sort(function (a, b) { return b.age - a.age; });
 console.log(sortedOlderStudents);
-var studentName = students.map(function (student) { return student.isStudent == false; });
+var studentName = students.map(function (student) { return (__assign(__assign({}, student), { isStudent: false })); });
 console.log(studentName);
