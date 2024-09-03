1a8629db3fa78d6a83cfdd7255136ec941cc30f0
Toharkenin <109432113+Toharkenin@users.noreply.github.com>
2024-09-03 17:21:52 +0300

    home-puzzle

--- 04 - Typescript/06 - DOM manipulations - 30-08-2024/HomePuzzles/Tohar/dist/index.js
+++ 04 - Typescript/06 - DOM manipulations - 30-08-2024/HomePuzzles/Tohar/dist/index.js
@@ -12,13 +12,13 @@ var computersList = [
     { id: 1, name: "Zenebook", price: 7700, sale: false },
     { id: 2, name: "Vivobook", price: 8000, sale: false },
     { id: 3, name: "Chromebook", price: 1000, sale: false },
-    { id: 4, name: "ExpertBook", price: 800, sale: false },
+    { id: 4, name: "ExpertBook", price: 800, sale: true },
     { id: 5, name: "Next-Gen Console", price: 10000, sale: true },
     { id: 6, name: "TUF Gaming VG28UQL1A", price: 10000, sale: true },
     { id: 7, name: "ASUS BR1104C", price: 900, sale: false },
     { id: 8, name: "ASUS BR1100F", price: 1100, sale: true },
     { id: 9, name: "BE24EQK", price: 2500, sale: true },
-    { id: 10, name: "ProArt", price: 750, sale: false },
+    { id: 10, name: "ProArt", price: 750, sale: true },
 ];
 function renderNavbar() {
     try {
@@ -45,7 +45,10 @@ function renderComputersList() {
         computersList.forEach((function (computer) {
             var computerItem = document.createElement("div");
             if (computer.price <= 1000) {
-                computerItem.innerHTML = "\n            <h1>" + computer.name + "</h1>\n            <h2>$" + computer.price + "</h2>\n            <button>Show More</button>\n            ";
+                computerItem.innerHTML = "\n                <h1>" + computer.name + "</h1>\n                <h2>$" + computer.price + "</h2>\n                <button>Show More</button>\n            ";
+                if (computer.sale) {
+                    computerItem.innerHTML = "\n                    <h1>" + computer.name + "</h1>\n                    <h2>$" + computer.price + "</h2>\n                    <button>Show More</button>\n                    <img src=\"https://static.vecteezy.com/system/resources/thumbnails/016/327/494/small_2x/sale-badge-tag-3d-icon-render-illustration-png.png\" />\n                ";
+                }
             }
             computerItem.classList.add("navbarItem");
             computersElement_1.appendChild(computerItem);
