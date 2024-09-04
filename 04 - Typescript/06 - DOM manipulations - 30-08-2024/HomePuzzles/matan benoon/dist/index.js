9de070c0e4a36028120d54d80c36af0effb99d45
Tal <tal.yaron@gmail.com>
2024-09-04 20:03:07 +0300

    fixed

--- 04 - Typescript/06 - DOM manipulations - 30-08-2024/HomePuzzles/matan benoon/dist/index.js
+++ 04 - Typescript/06 - DOM manipulations - 30-08-2024/HomePuzzles/matan benoon/dist/index.js
@@ -8,110 +8,117 @@ var computers = [
     {
         name: ' asus screen 1700*1500 ',
         price: 1100,
-        id: 1298,
+        id: 1254,
         sale: true
     },
     {
         name: ' asus screen 1600*1250 ',
         price: 1200,
-        id: 1298,
+        id: 78967864,
         sale: false
     },
     {
         name: ' asus screen 1200*800 ',
         price: 900,
-        id: 1298,
+        id: 868456645,
         sale: true
     },
     {
         name: ' asus screen 1100*600 ',
         price: 700,
-        id: 1298,
+        id: 456786783,
         sale: true
     },
     {
         name: ' asus screen 1000*500 ',
         price: 600,
-        id: 1298,
+        id: 48646456776,
         sale: false
     },
     {
         name: ' asus screen 1920*1490 ',
         price: 1700,
-        id: 1298,
+        id: 6456345646,
         sale: false
     },
     {
         name: ' asus screen 1600*1100 ',
         price: 999,
-        id: 1298,
+        id: 87634534345,
         sale: true
     },
     {
         name: ' asus screen 890*600 ',
         price: 400,
-        id: 1298,
+        id: 456456456,
         sale: true
     },
     {
         name: ' asus screen 3000*1490 ',
         price: 1900,
-        id: 1298,
+        id: 48645645,
         sale: false
     },
 ];
 var navbar = [
     {
-        name: 'allcomputer'
+        name: 'allcomputer',
+        link: '/all-computers'
     },
     {
-        name: 'computers'
+        name: 'computers',
+        link: '/computers'
     },
     {
-        name: 'phones'
+        name: 'phones',
+        link: '/phones'
     },
     {
-        name: 'tablet'
+        name: 'tablet',
+        link: '/tablet'
     },
     {
-        name: 'screen'
+        name: 'screen',
+        link: '/screen'
     },
 ];
-function navbarSelectId() {
-    var navbarItems = document.querySelector('#navbar');
+//view
+function renderNav() {
     try {
-        if (!navbarSelectId)
+        var navbarItems_1 = document.querySelector('#navbar');
+        if (!navbarItems_1)
             throw new Error("could not found");
         navbar.forEach(function (item) {
-            var menuList = document.createElement('div');
-            menuList.innerHTML =
-                " <a>" + item.name;
-            menuList.classList.add("navlist");
-            navbarItems.appendChild(menuList);
+            var navItem = document.createElement('div');
+            navItem.innerHTML =
+                "<a href=\"" + item.link + "\">" + item.name + "</a>";
+            navItem.classList.add("nav-item");
+            navbarItems_1.appendChild(navItem);
         });
     }
     catch (e) {
         console.error(e);
     }
 }
-function computersSelcetId() {
+renderNav();
+function renderComputers() {
     var computersItem = document.querySelector('#computers');
     try {
-        if (!computersSelcetId)
+        if (!computersItem)
             throw new Error("could not found");
         computers.forEach(function (itemComp) {
-            var computersList = document.createElement('div');
+            var computerItem = document.createElement('div');
             if (itemComp.price <= 1000 && itemComp.sale) {
-                computersList.innerHTML =
-                    "<h1> " + itemComp.name + "</h1>,\n            <h2> " + itemComp.price + "</h2>\n            <button> ' show more ' </button>\n            <img src= \"https://m.media-amazon.com/images/I/71ehzrGUO7L._AC_UF1000,1000_QL80_.jpg\">\n            ";
+                computerItem.innerHTML =
+                    " <div class=\"computer-item__info\">\n                <div class=\"computer-item__title\"> " + itemComp.name + "</div>\n            <div class=\"computer-item__price\">$" + itemComp.price + "</div>\n            <button>show more</button>\n            </div>\n            <img src= \"https://m.media-amazon.com/images/I/71ehzrGUO7L._AC_UF1000,1000_QL80_.jpg\">\n            " + (itemComp.sale === true ? "<div class='sale'>On Sale </div>" : "") + "\n            ";
             }
-            computersList.classList.add("computer_list");
-            computersItem.appendChild(computersList);
+            computerItem.classList.add("computer-item");
+            computersItem.appendChild(computerItem);
         });
     }
     catch (e) {
         console.error(e);
     }
 }
-computersSelcetId();
-navbarSelectId();
+renderComputers();
+// navbarSelectId();
--- 04 - Typescript/06 - DOM manipulations - 30-08-2024/HomePuzzles/matan benoon/index.ts
+++ 04 - Typescript/06 - DOM manipulations - 30-08-2024/HomePuzzles/matan benoon/index.ts
@@ -1,16 +1,17 @@
-interface Nav{
-    name:string;
+interface Nav {
+    name: string;
+    link: string;
 }
 
-interface Computers{
-    name:string;
-    price:number;
-    id:number;
-    sale:boolean;
+interface Computers {
+    name: string;
+    price: number;
+    id: number;
+    sale: boolean;
 }
 
 
-const computers:Computers []= [
+const computers: Computers[] = [
     {
         name: ' asus screen 1920*1490 ',
         price: 1480,
@@ -20,128 +21,137 @@ const computers:Computers []= [
     {
         name: ' asus screen 1700*1500 ',
         price: 1100,
-        id: 1298,
+        id: 1254,
         sale: true,
     },
     {
         name: ' asus screen 1600*1250 ',
         price: 1200,
-        id: 1298,
+        id: 78967864,
         sale: false,
     },
     {
         name: ' asus screen 1200*800 ',
         price: 900,
-        id: 1298,
+        id: 868456645,
         sale: true,
     },
     {
         name: ' asus screen 1100*600 ',
         price: 700,
-        id: 1298,
+        id: 456786783,
         sale: true,
     },
     {
         name: ' asus screen 1000*500 ',
         price: 600,
-        id: 1298,
+        id: 48646456776,
         sale: false,
     },
     {
         name: ' asus screen 1920*1490 ',
         price: 1700,
-        id: 1298,
+        id: 6456345646,
         sale: false,
     },
     {
         name: ' asus screen 1600*1100 ',
         price: 999,
-        id: 1298,
+        id: 87634534345,
         sale: true,
     },
     {
         name: ' asus screen 890*600 ',
         price: 400,
-        id: 1298,
+        id: 456456456,
         sale: true,
     },
     {
         name: ' asus screen 3000*1490 ',
         price: 1900,
-        id: 1298,
+        id: 48645645,
         sale: false,
     },
 ]
 
-const navbar:Nav[]= [
+const navbar: Nav[] = [
     {
-        name:'allcomputer',
+        name: 'allcomputer',
+        link: '/all-computers'
     },
     {
-        name:'computers',
+        name: 'computers',
+        link: '/computers'
     },
     {
-        name:'phones',
+        name: 'phones',
+        link: '/phones'
     },
     {
-        name:'tablet',
+        name: 'tablet',
+        link: '/tablet'
     },
     {
-        name:'screen',
+        name: 'screen',
+        link: '/screen'
     },
 ]
 
+//view
+function renderNav(): void {
+    try {
+        const navbarItems = document.querySelector('#navbar') as HTMLElement
+        if (!navbarItems) throw new Error("could not found");
 
-function navbarSelectId () :void{
-    const navbarItems = document.querySelector ('#navbar') as HTMLElement
-try{
-    if (!navbarSelectId) throw new Error("could not found");
+        navbar.forEach(item => {
+            const navItem = document.createElement('div');
 
-    navbar.forEach(item =>{
-        const menuList = document.createElement('div');
+            navItem.innerHTML =
+                `<a href="${item.link}">${item.name}</a>`;
 
-        menuList.innerHTML = 
-        ` <a>${item.name}`;
+            navItem.classList.add("nav-item");
+            navbarItems.appendChild(navItem);
 
-        menuList.classList.add("navlist");
-        navbarItems.appendChild(menuList);
+        });
 
-    });
+    } catch (e) {
+        console.error(e);
+    }
 
-}catch(e){
-    console.error(e);
 }
+renderNav();
 
-}
+function renderComputers(): void {
+    const computersItem = document.querySelector('#computers') as HTMLElement
+    try {
+        if (!computersItem) throw new Error("could not found");
 
-function computersSelcetId () :void{
-    const computersItem = document.querySelector ('#computers') as HTMLElement
-try{
-    if (!computersSelcetId) throw new Error("could not found");
+        computers.forEach(itemComp => {
+            const computerItem = document.createElement('div');
+            if (itemComp.price <= 1000 && itemComp.sale) {
 
-    computers.forEach(itemComp =>{
-        const computersList = document.createElement ('div');
-        if (itemComp.price <= 1000 && itemComp.sale){
-
-            computersList.innerHTML = 
-            `<h1> ${itemComp.name}</h1>,
-            <h2> ${itemComp.price}</h2>
-            <button> ' show more ' </button>
+                computerItem.innerHTML =
+                    ` <div class="computer-item__info">
+                <div class="computer-item__title"> ${itemComp.name}</div>
+            <div class="computer-item__price">$${itemComp.price}</div>
+            <button>show more</button>
+            </div>
             <img src= "https://m.media-amazon.com/images/I/71ehzrGUO7L._AC_UF1000,1000_QL80_.jpg">
+            ${itemComp.sale === true ? "<div class='sale'>On Sale </div>" : ""}
             `
-        }
-        
-        
-        computersList.classList.add("computer_list");
-        computersItem.appendChild(computersList);
+            }
 
-    })
 
-}catch(e){
-    console.error(e);
+            computerItem.classList.add("computer-item");
+            computersItem.appendChild(computerItem);
+
+        })
+
+    } catch (e) {
+        console.error(e);
+    }
+
 }
 
-}
-
-computersSelcetId ();
-navbarSelectId();
+renderComputers();
+// navbarSelectId();
