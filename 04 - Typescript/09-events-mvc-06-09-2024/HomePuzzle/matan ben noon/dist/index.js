d348dc725ad1cc03895f531087ebe3d1c1f3b0f4
Matan Benoon <139704968+matanbn1121@users.noreply.github.com>
2024-09-06 17:10:01 +0300

    homepuzzle

---
+++ 04 - Typescript/09-events-mvc-06-09-2024/HomePuzzle/matan ben noon/dist/index.js
@@ -0,0 +1,37 @@
+var movies = [];
+function main() {
+    try {
+        var formId = document.getElementById('the-form');
+        if (!formId)
+            throw new Error('Form not found');
+        formId.addEventListener('submit', movieInput);
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
+function movieInput(event) {
+    event.preventDefault();
+    var form = event.target;
+    var title = form.title.value;
+    var year = form.year.value;
+    var author = form.author.value;
+    var rating = form.rating.value;
+    var imgUrl = form.imgUrl.value;
+    if (title && year && author && rating && imgUrl) {
+        movies.push({ name: title, year: year, author: author, rating: rating, imgUrl: imgUrl });
+        movies.sort(function (a, b) { return b.rating - a.rating; });
+    }
+    renderMovies();
+}
+function deleteMovie(movieName) {
+    var index = movies.findIndex(function (movie) { return movie.name === movieName; });
+    if (index !== -1) {
+        movies.splice(index, 1);
+        renderMovies();
+    }
+}
+function renderMovies() {
+    var movieList = document.getElementById('movie-list');
+    movieList.innerHTML = movies.map(function (movie) { return " <li>\n                <h2>" + movie.name + "</h2>\n                <p>Author: " + movie.author + "</p>\n                <p>Year: " + movie.year + "</p>\n                <p>Rating: " + movie.rating + "</p>\n                <img src=\"" + movie.imgUrl + "\" alt=\"" + movie.name + "\" style=\"max-width: 200px; height: auto;\">\n                <button onclick=\"deleteMovie('" + movie.name + "')\">Delete</button>\n                 </li>"; }).join('');
+}
---
+++ 04 - Typescript/09-events-mvc-06-09-2024/HomePuzzle/matan ben noon/index.ts
@@ -0,0 +1,74 @@
+interface Movies{
+    name: string;
+    author: string;
+    year: number;
+    rating: number;
+    imgUrl:string
+}
+
+
+const movies:Movies[]= [];
+
+function main():void{
+
+    try{
+    const formId = document.getElementById('the-form') as HTMLFormElement;
+    if(!formId) throw new Error('Form not found');
+
+    formId.addEventListener('submit', movieInput  )
+
+    }catch(e){
+        console.log(e);
+}
+}
+
+
+
+function movieInput(event:Event):void{
+    event.preventDefault();
+    const form:any = event.target as HTMLFormElement;
+
+    const title = form.title.value;
+        const year = form.year.value;
+        const author = form.author.value;
+        const rating = form.rating.value;
+        const imgUrl = form.imgUrl.value;
+
+
+    if(title && year && author&& rating && imgUrl){
+
+        movies.push({ name: title, year, author, rating, imgUrl });
+
+        movies.sort((a, b) => b.rating - a.rating);
+
+        
+    }
+
+    renderMovies()
+
+}
+
+
+function deleteMovie(movieName: string): void {
+    const index = movies.findIndex(movie => movie.name === movieName);
+    if (index !== -1) {
+        movies.splice(index, 1); 
+        renderMovies();
+    }
+}
+
+
+function renderMovies():void{
+
+    const movieList = document.getElementById('movie-list') as HTMLElement;
+    movieList.innerHTML = movies.map(movie => ` <li>
+                <h2>${movie.name}</h2>
+                <p>Author: ${movie.author}</p>
+                <p>Year: ${movie.year}</p>
+                <p>Rating: ${movie.rating}</p>
+                <img src="${movie.imgUrl}" alt="${movie.name}" style="max-width: 200px; height: auto;">
+                <button onclick="deleteMovie('${movie.name}')">Delete</button>
+                 </li>`).join('');
+}
+
+
