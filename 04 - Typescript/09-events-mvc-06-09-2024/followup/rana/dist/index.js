b716bfba1530cf9f7a14a7767a911fed4b553503
Ranazidan1 <halabyrana8@gmail.com>
2024-09-06 11:38:47 +0300

    rana3

--- 04 - Typescript/09-events-mvc-06-09-2024/followup/rana/dist/index.js
+++ 04 - Typescript/09-events-mvc-06-09-2024/followup/rana/dist/index.js
@@ -33,4 +33,16 @@ function handleInput(event) {
     catch (error) {
         console.error(error);
     }
+    renderWords();
+}
+function renderWords() {
+    try {
+        var wordList = document.getElementById('words');
+        if (!wordList)
+            throw new Error('Word list not found');
+        wordList.innerHTML = words.map(function (words) { return "<li>" + words + "</li>"; }).join('');
+    }
+    catch (error) {
+        console.error(error);
+    }
 }
