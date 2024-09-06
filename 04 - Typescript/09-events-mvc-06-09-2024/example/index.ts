db9f5b4fe256fbdc64b5603c3fb3e5e6ed1ac556
Tal <tal.yaron@gmail.com>
2024-09-06 09:38:36 +0300

    followup 1

---
+++ 04 - Typescript/09-events-mvc-06-09-2024/example/index.ts
@@ -0,0 +1,54 @@
+const words: string[] = [];
+
+function handleStart(): void {
+    console.log('Start');
+    try {
+        //add event listener to input field
+        const inputWord = document.getElementById('input-word') as HTMLInputElement;
+        if (!inputWord) throw new Error('Input element not found');
+
+        inputWord.addEventListener("keyup", handleInput) 
+
+    } catch (error) {
+        console.error(error);
+
+    }
+}
+
+
+function handleInput(event:any): void {
+
+    //when key is enter --> add  word to array
+    if (event.key === 'Enter') {
+        if (event.target instanceof HTMLInputElement) {
+            const newWord = event.target.value;
+            if (newWord) {
+                
+                //changed model
+                words.push(newWord);
+                event.target.value = '';
+
+                //change view accordingly
+                renderWords();
+            }
+        }
+    }
+
+}
+//on event input -> add word to array -> print array to DOM
+
+function renderWords(): void {
+    try {
+        //catch the dom element
+        const wordList = document.getElementById('words') as HTMLUListElement;
+        if (!wordList) throw new Error('Word list not found');
+        //render list to dom
+
+        wordList.innerHTML = words.map((word) => `<li>${word}</li>`).join('');
+
+
+    } catch (error) {
+        console.error(error);
+
+    }
+}
---
+++ 04 - Typescript/09-events-mvc-06-09-2024/followup/instructions.md
@@ -0,0 +1,8 @@
+# Followup for event -> DOM
+
+## Instructions
+
+### Create onload handler
+  - add input field to the HTML
+  - add event listener to the input field
+  - create event handler for the input field -> write event to console. 
