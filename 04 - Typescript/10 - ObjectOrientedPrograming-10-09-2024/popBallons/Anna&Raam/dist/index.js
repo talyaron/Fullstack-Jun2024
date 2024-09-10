9b0a2b53434356647e5dff1e60effe20f2d2841e
Annapetr0 <45635544+AnnaPetr0@users.noreply.github.com>
2024-09-10 20:57:50 +0300

    popBaloons

---
+++ 04 - Typescript/10 - ObjectOrientedPrograming-10-09-2024/popBallons/Anna&Raam/dist/index.js
@@ -0,0 +1,26 @@
+var Baloon = /** @class */ (function () {
+    function Baloon(imageUrl) {
+        this.imageUrl = imageUrl;
+        this.id = "id - " + crypto.randomUUID();
+    }
+    Baloon.prototype.setImageURL = function (imageUrl) {
+        this.imageUrl = imageUrl;
+    };
+    return Baloon;
+}());
+var annaBallon = new Baloon('./dist/image/Purple_Balloon.png');
+var raamBallon = new Baloon('./dist/image/blue-baloon-removebg-preview.png');
+function renderNewBallon(baloon) {
+    var baloonElement = document.createElement('img');
+    baloonElement.src = baloon.imageUrl;
+    baloonElement.id = baloon.id;
+    baloonElement.style.top = 0 + 'px';
+    baloonElement.style.left = 0 + 'px';
+    document.body.appendChild(baloonElement);
+    baloonElement.addEventListener('click', function () {
+        baloon.setImageURL('./dist/image/purpleblowup.png');
+    });
+}
+renderNewBallon(annaBallon);
+console.log(annaBallon);
+//renderNewBallon(raamBallon);
