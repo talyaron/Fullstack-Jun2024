6d5420bd2fd75fc964431ee4f5161c620f3276ff
Elizigi <elizigi876@gmail.com>
2024-09-10 20:25:28 +0300

    Update

--- 04 - Typescript/10 - ObjectOrientedPrograming-10-09-2024/popBallons/EliToharNatalie/dist/index.js
+++ 04 - Typescript/10 - ObjectOrientedPrograming-10-09-2024/popBallons/EliToharNatalie/dist/index.js
@@ -6,4 +6,27 @@ var Ballon = /** @class */ (function () {
     }
     return Ballon;
 }());
-var ballon1 = new Ballon("", "", "");
+var ballon1 = new Ballon("", "./dist/images/balloon.png", "");
+var ballon2 = new Ballon("", "./dist/images/balloon.png", "");
+var ballon3 = new Ballon("", "./dist/images/balloon.png", "");
+function main() {
+    renderBallon(ballon2);
+}
+function renderBallon(ballon) {
+    try {
+        var pageElement = document.getElementById("page");
+        if (!pageElement)
+            throw new Error("no pag found");
+        var ballonElement_1 = document.createElement("img");
+        ballonElement_1.src = ballon.ballonImgUrl;
+        ballonElement_1.id = ballon.id;
+        ballonElement_1.addEventListener("click", function () {
+            ballonElement_1.src = ballon.ExplosionUrl;
+        });
+        pageElement.appendChild(ballonElement_1);
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+function pop(event) { }
