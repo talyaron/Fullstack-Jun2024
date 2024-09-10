3d2f0a7d1d59127e8539fb9794de2d06ab24312e
alexis <gog20@icloud.com>
2024-09-10 21:02:10 +0300

    cp

---
+++ 04 - Typescript/10 - ObjectOrientedPrograming-10-09-2024/popBallons/alexis/dist/index.js
@@ -0,0 +1,51 @@
+var Baloon = /** @class */ (function () {
+    function Baloon(imageUrl, imageExplode, baloonElement) {
+        try {
+            this.imageUrl = imageUrl;
+            this.imageExplode = imageExplode;
+            this.id = crypto.randomUUID();
+            if (!baloonElement) {
+                throw new Error("baloon element is not found");
+            }
+            this.baloonElement = baloonElement;
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+    Baloon.prototype.renderBaloon = function () {
+        this.baloonElement.src = this.imageUrl;
+        this.baloonElement.classList.add('baloon');
+    };
+    Baloon.prototype.explodeBaloon = function () {
+        this.baloonElement.src = this.imageExplode;
+    };
+    return Baloon;
+}());
+var baloon = document.getElementById("baloon");
+if (!baloon) {
+    throw new Error("no baloon");
+}
+var baloonOne = new Baloon("../baloon1", "../baloon2", baloon);
+function main() {
+    try {
+        if (!baloon) {
+            throw new Error("no baloon");
+        }
+        baloonOne.baloonElement.addEventListener("click", explosionEvent);
+        baloonOne.renderBaloon();
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+function explosionEvent() {
+    baloonOne.explodeBaloon();
+    console.log(baloonOne);
+}
+// function renderBaloon(baloonOne) {
+//   const baloon = document.createElement("img");
+//   baloon.src = baloonOne.imageUrl;
+//   baloon.id = baloonOne.id;
+// }
+main();
