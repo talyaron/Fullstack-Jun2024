eb2c3efa038a521c1b4af11a0c79164ee1a4a59c
Yosef Ibrahim <yosefib88@gmail.com>
2024-09-13 09:22:26 +0300

    סדרתי את הקובץ

--- 04 - Typescript/10 - ObjectOrientedPrograming-10-09-2024/popBallons/yosef/dist/index.js
+++ 04 - Typescript/10 - ObjectOrientedPrograming-10-09-2024/popBallons/yosef/dist/index.js
@@ -1,57 +1,38 @@
+var boom = 'https://pic1.yitweb.co.il/picserver/mynet/crop_images/2023/11/13/HJNPSnkNa/HJNPSnkNa_0_0_640_360_0_large.jpg';
+var no_boom = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7Ec4AyjMzCAhhRfgWLLCCa1edXtULgPt64w&s';
 var Ballon = /** @class */ (function () {
     function Ballon(imageURL) {
-        this.imageURL = imageURL;
+        var _this = this;
+        this.Ballon_imageURL = imageURL;
         this.id = Math.random();
+        this.element = document.createElement('img');
+        this.element.src = no_boom;
+        this.renderNewBallon();
+        this.element.addEventListener('click', function (event) {
+            _this.element.src = boom;
+        });
     }
     Ballon.prototype.explode_ballon = function () {
-        this.imageURL = 'https://pic1.yitweb.co.il/picserver/mynet/crop_images/2023/11/13/HJNPSnkNa/HJNPSnkNa_0_0_640_360_0_large.jpg';
+        this;
+        this.Ballon_imageURL = boom;
+    };
+    Ballon.prototype.renderNewBallon = function () {
+        var box = document.getElementById('box');
+        if (!box)
+            throw new Error('Box not found.');
+        this.element = document.createElement('img');
+        this.element.src = no_boom;
+        this.element.id = String(this.id);
+        this.element.style.position = 'absolute';
+        box.appendChild(this.element);
     };
     return Ballon;
 }());
-var ballon = new Ballon('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7Ec4AyjMzCAhhRfgWLLCCa1edXtULgPt64w&s');
-console.log(ballon);
 function main2() {
     try {
-        //get field, andd add event listener
-        var box = document.getElementById('box');
-        if (!box) {
-            throw new Error('Field not found');
-        }
-        box.addEventListener('click', handleMove);
+        var ballon = new Ballon(boom);
     }
     catch (error) {
-        console.error(error);
+        console.error('An error occurred:', error);
     }
 }
-function renderNewBallon(ballon) {
-    try {
-        var BallonElement = document.createElement('img');
-        BallonElement.src = ballon.imageURL;
-        var box = document.getElementById('box');
-        if (!box) {
-            throw new Error('Box not found');
-        }
-        box.appendChild(BallonElement);
-        var field = document.getElementById('field');
-    }
-    catch (error) {
-        console.error(error);
-    }
-}
-function handleMove(event) {
-    try {
-        var box = document.getElementById('box');
-        if (!box) {
-            throw new Error('Box not found');
-        }
-        ballon.imageURL = 'https://pic1.yitweb.co.il/picserver/mynet/crop_images/2023/11/13/HJNPSnkNa/HJNPSnkNa_0_0_640_360_0_large.jpg';
-        renderNewBallon(ballon);
-        ballon.explode_ballon();
-        console.log(ballon.imageURL);
-    }
-    catch (error) {
-        console.error(error);
-    }
-}
-main2();
-renderNewBallon(ballon);
