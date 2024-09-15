538748115069e658bbf3f4946201b99532752d06
Annapetr0 <45635544+AnnaPetr0@users.noreply.github.com>
2024-09-15 16:58:18 +0300

    nbnb

--- 04 - Typescript/11 - OOP2/Project/IcyTower(GalitAnna)/dist/index.js
+++ 04 - Typescript/11 - OOP2/Project/IcyTower(GalitAnna)/dist/index.js
@@ -1,4 +1,62 @@
 var character = './images/character1.png';
+var Player = /** @class */ (function () {
+    function Player(x, y, imageUrl) {
+        this.positionX = x;
+        this.positionY = y;
+        this.imageUrl = './images/character1.png';
+    }
+    Object.defineProperty(Player.prototype, "getPositionX", {
+        get: function () {
+            return this.positionX;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Player.prototype, "getPositionY", {
+        get: function () {
+            return this.positionY;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Player.prototype, "getImageUrl", {
+        get: function () {
+            return this.imageUrl;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Player.prototype, "setPositionX", {
+        set: function (x) {
+            this.positionX = x;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Player.prototype, "setPositionY", {
+        set: function (y) {
+            this.positionY = y;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(Player.prototype, "setImageUrl", {
+        set: function (image) {
+            this.imageUrl = image;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Player.prototype.renderPlayer = function (mainElement) {
+        try {
+            if (!mainElement)
+                throw new Error('Main element is required');
+        }
+        catch (error) {
+        }
+    };
+    return Player;
+}());
 var Step = /** @class */ (function () {
     function Step(length, height) {
         try {
--- 04 - Typescript/11 - OOP2/Project/IcyTower(GalitAnna)/index.ts
+++ 04 - Typescript/11 - OOP2/Project/IcyTower(GalitAnna)/index.ts
@@ -1,5 +1,50 @@
 const character: string = './images/character1.png';
 
+class Player {
+    private positionX: number;
+    private positionY : number;
+    private imageUrl : string;
+
+    constructor (x : number , y : number, imageUrl : string){
+        this.positionX = x;
+        this.positionY = y;
+        this.imageUrl = './images/character1.png';
+    }
+
+    get getPositionX(){
+        return this.positionX;
+    }
+
+    get getPositionY(){
+        return this.positionY;
+    }
+
+    get getImageUrl(){
+        return this.imageUrl;
+    }
+
+    set setPositionX(x : number){
+        this.positionX = x;
+    }
+
+    set setPositionY(y : number){
+        this.positionY = y;
+    }
+
+    set setImageUrl(image : string){
+        this.imageUrl = image;
+    }
+
+    renderPlayer(mainElement: HTMLDivElement){
+        try {
+            if(!mainElement) throw new Error('Main element is required');
+            
+        } catch (error) {
+            
+        }
+    }
+}
+
 class Step {
 private length: number;
 private height: number;
@@ -9,9 +54,11 @@ constructor (length: number, height: number) {
     try {
     this.length= length;
     this.height=height;
-}
- catch (error) {
+    }catch (error) {
     console.error(error);
-}
-}
+    }
+    }
+
+
+
 }
