118fdb159c76432833a92a5656f01953b4c9df0c
Toharkenin <109432113+Toharkenin@users.noreply.github.com>
2024-09-17 21:36:52 +0300

    start

--- 04 - Typescript/11 - OOP2/Project/FlappyBird/dist/index.js
+++ 04 - Typescript/11 - OOP2/Project/FlappyBird/dist/index.js
@@ -1,8 +1,36 @@
 var Bird = /** @class */ (function () {
-    function Bird(position) {
-        this.id = "id" + crypto.randomUUID();
+    function Bird(position, velocity, gravity) {
+        this.id = "-id" + crypto.randomUUID();
         this.position = position;
-        this.imgUrl = "";
+        this.imgUrl = "./dist/images/bird1.png";
+        this.gravity = gravity;
+        this.velocity = velocity;
+        this.renderBird();
     }
+    Bird.prototype.getElement = function () {
+        return this.element;
+    };
+    //methods 
+    Bird.prototype.renderBird = function () {
+        console.log("in render");
+        try {
+            this.element = document.createElement('img');
+            this.element.src = this.imgUrl;
+            this.element.id = this.id;
+            this.element.style.position = 'absolute';
+            this.element.style.left = this.position.x + '%';
+            this.element.style.top = this.position.y + '%';
+            this.element.classList.add('bird');
+            document.body.appendChild(this.element);
+        }
+        catch (e) {
+            console.error(e);
+        }
+    };
     return Bird;
 }());
+function main() {
+    new Bird({ x: 50, y: 50 }, 0, 0);
+    console.log("i");
+}
+;
--- 04 - Typescript/11 - OOP2/Project/FlappyBird/index.ts
+++ 04 - Typescript/11 - OOP2/Project/FlappyBird/index.ts
@@ -1,16 +1,52 @@
+interface Position {
+    x: number;
+    y: number;
+}
+
 class Bird {
     private id: string;
-    private position: number;
+    private position: Position;
     private velocity: number;
     private imgUrl: string;
     private gravity: number;
+    private element: HTMLImageElement;
 
-    constructor(position: number) {
-        this.id = `id${crypto.randomUUID()}`;
+    constructor (position: Position, velocity: number, gravity: number) {
+        this.id = `-id${crypto.randomUUID()}`;
         this.position = position;
-        this.imgUrl = "";
+        this.imgUrl = "./dist/images/bird1.png";
+        this.gravity = gravity;
+        this.velocity = velocity;
+        this.renderBird();
+    }
+
+    getElement(): HTMLImageElement
+    {
+        return this.element;
     }
 
     //methods 
-    
+    renderBird(): void {
+        console.log("in render");
+        try {
+            this.element = document.createElement('img');
+            this.element.src = this.imgUrl;
+            this.element.id = this.id;
+            this.element.style.position = 'absolute';
+            this.element.style.left = this.position.x + '%';
+            this.element.style.top = this.position.y + '%';
+            this.element.classList.add('bird');
+            document.body.appendChild(this.element);
+            
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+
 }
+
+function main():void {
+    new Bird({x: 50, y: 50}, 0, 0);
+    console.log("i");
+};
