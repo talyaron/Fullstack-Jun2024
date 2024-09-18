f8d5ce5d3019de7e425f55a3332f0f1f76ad7923
Toharkenin <109432113+Toharkenin@users.noreply.github.com>
2024-09-18 18:02:21 +0300

    flappybirdmove

--- 04 - Typescript/11 - OOP2/Project/FlappyBird/dist/index.js
+++ 04 - Typescript/11 - OOP2/Project/FlappyBird/dist/index.js
@@ -1,27 +1,60 @@
 var Bird = /** @class */ (function () {
     function Bird(position, velocity, gravity) {
+        var _this = this;
         this.id = "-id" + crypto.randomUUID();
         this.position = position;
-        this.imgUrl = "./dist/images/bird1.png";
-        this.flyingBirdImgUrl = "./dist/images/fly.png";
+        this.imgUrl = "./dist/images/bird-down.png";
+        this.flyingBirdImgUrl = "./dist/images/bird-up.png";
         this.gravity = gravity;
         this.velocity = velocity;
         this.renderBird();
+        this.isFlying = false;
+        window.addEventListener('keydown', function (event) {
+            if (event.code === 'Space') {
+                _this.handlePressKeyDown();
+            }
+        });
+        window.addEventListener('keyup', function (event) {
+            if (event.code === 'Space') {
+                _this.handlePressKeyUp();
+            }
+        });
     }
-    // getElement(): HTMLImageElement
-    // {
-    //     return this.element;
-    // }
+    //GETTER
+    Bird.prototype.getElement = function () {
+        return this.element;
+    };
     //methods 
+    Bird.prototype.initialPosition = function () {
+        this.position.x = 40;
+    };
+    Bird.prototype.moveWings = function () {
+        var _this = this;
+        var element = this.getElement();
+        setInterval(function () {
+            element.src = _this.imgUrl;
+        }, 500);
+        setTimeout(function () {
+            setInterval(function () {
+                element.src = _this.flyingBirdImgUrl;
+            }, 500);
+        }, 200);
+        return element;
+    };
     Bird.prototype.renderBird = function () {
         console.log("in render");
         try {
             this.element = document.createElement('img');
-            this.element.src = this.imgUrl;
-            this.element.id = this.id;
+            if (!this.isFlying) {
+                this.moveWings();
+            }
+            else {
+                this.element.src = this.imgUrl;
+            }
+            // this.element.id = this.id;
             this.element.style.position = 'absolute';
-            this.element.style.left = this.position.x + '%';
-            this.element.style.top = this.position.y + '%';
+            this.element.style.left = this.position.x + 'px';
+            this.element.style.top = this.position.y + 'px';
             this.element.classList.add('bird');
             document.body.appendChild(this.element);
         }
@@ -29,10 +62,15 @@ var Bird = /** @class */ (function () {
             console.error(e);
         }
     };
+    Bird.prototype.handlePressKeyDown = function () {
+        console.log('pressed');
+        this.position.y = this.position.y + 30;
+        this.element.style.left = this.position.x + 'px';
+        this.element.style.top = this.position.y + 'px';
+    };
     return Bird;
 }());
 function main() {
-    new Bird({ x: 50, y: 50 }, 0, 0);
-    console.log("i");
+    new Bird({ x: 300, y: 300 }, 0, 0);
 }
 ;
--- 04 - Typescript/11 - OOP2/Project/FlappyBird/index.ts
+++ 04 - Typescript/11 - OOP2/Project/FlappyBird/index.ts
@@ -11,35 +11,68 @@ class Bird {
     private gravity: number;
     private element: HTMLImageElement;
     private flyingBirdImgUrl: string;
+    isFlying: boolean;
 
     constructor (position: Position, velocity: number, gravity: number) {
         this.id = `-id${crypto.randomUUID()}`;
         this.position = position;
-        this.imgUrl = "./dist/images/bird1.png";
-        this.flyingBirdImgUrl = "./dist/images/fly.png";
+        this.imgUrl = "./dist/images/bird-down.png";
+        this.flyingBirdImgUrl = "./dist/images/bird-up.png";
         this.gravity = gravity;
         this.velocity = velocity;
         this.renderBird();
+        this.isFlying = false;
+
+        window.addEventListener('keydown', (event: KeyboardEvent) => {
+            if (event.code === 'Space'){
+                this.handlePressKeyDown();
+            }
+        });
+
+        window.addEventListener('keyup', (event: KeyboardEvent) => {
+            if (event.code === 'Space'){
+                this.handlePressKeyUp();
+            }
+        });
     }
-    
-    initialPosition(): void {
 
-    }
-
-    moveWings(): void {
-
+     //GETTER
+    getElement(): HTMLImageElement {
+        return this.element;
     }
 
     //methods 
+
+    initialPosition(): void {
+        this.position.x = 40;
+    }
+
+    moveWings(): HTMLImageElement {
+        const element = this.getElement();
+        setInterval(() => {
+            element.src = this.imgUrl;
+        }, 500);
+        setTimeout(() => {
+            setInterval(() => {
+                element.src = this.flyingBirdImgUrl
+            }, 500);
+          }, 200);
+        return element;
+    }
+
     renderBird(): void {
         console.log("in render");
         try {
             this.element = document.createElement('img');
-            this.element.src = this.imgUrl;
-            this.element.id = this.id;
+            if (!this.isFlying) {
+                this.moveWings();
+            } else {
+                this.element.src = this.imgUrl;
+            }
+            // this.element.id = this.id;
             this.element.style.position = 'absolute';
-            this.element.style.left = this.position.x + '%';
-            this.element.style.top = this.position.y + '%';
+            this.element.style.left = this.position.x + 'px';
+            this.element.style.top = this.position.y + 'px';
             this.element.classList.add('bird');
             document.body.appendChild(this.element);
             
@@ -48,10 +81,23 @@ class Bird {
         }
     }
 
+    handlePressKeyDown(): void{
+        console.log('pressed');
 
+        this.position.y = this.position.y + 30;
+        
+        this.element.style.left = this.position.x + 'px';
+        this.element.style.top = this.position.y + 'px';
+    }
+
+    // handlePressKeyUp(): void {
+    //     this.position.y = this.position.y --;
+        
+    //     this.element.style.left = this.position.x + 'px';
+    //     this.element.style.top = this.position.y + 'px';
+    // }
 }
 
 function main():void {
-    new Bird({x: 50, y: 50}, 0, 0);
-    console.log("i");
+    new Bird({x: 300, y: 300}, 0, 0);
 };
