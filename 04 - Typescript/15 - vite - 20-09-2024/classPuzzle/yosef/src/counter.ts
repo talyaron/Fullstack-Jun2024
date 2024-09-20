ef64f382e66dd9d84d80718ff2a76b1f50558267
Yosef Ibrahim <yosefib88@gmail.com>
2024-09-20 12:56:39 +0300

    1
    111

--- 04 - Typescript/15 - vite - 20-09-2024/classPuzzle/yosef/src/counter.ts
+++ 04 - Typescript/15 - vite - 20-09-2024/classPuzzle/yosef/src/counter.ts
@@ -4,7 +4,7 @@ export function setupCounter(element: HTMLButtonElement) {
   let counter = 0
   const setCounter = (count: number) => {
     counter = count
-    element.innerHTML = `count is ${returnNambuer()}`
+    element.innerHTML = `count is by yosef: ${returnNambuer()}`
   }
   element.addEventListener('click', () => setCounter(counter + 1))
   setCounter(0)
