53a5408f6dc72a777a4277bce5b541459dee63b1
Yosef Ibrahim <yosefib88@gmail.com>
2024-09-20 11:47:28 +0300

    123
    123

---
+++ 04 - Typescript/15 - vite - 20-09-2024/classPuzzle/yosef/src/main.ts
@@ -0,0 +1,29 @@
+import './style.css'
+import typescriptLogo from './typescript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+import './views/view.ts'
+
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+    //   <img srlass="logo" alt="Vite logo" />
+    // </a>
+    // <a href="https://www.typescriptlang.org/" target="_blank">
+      // <img src=="logo vanilla" alt="TypeScript logo" />
+    </a>
+    <h1>Vite + TypeScript</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite and TypeScript logos to learn more
+    </p>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+alert("asd");
