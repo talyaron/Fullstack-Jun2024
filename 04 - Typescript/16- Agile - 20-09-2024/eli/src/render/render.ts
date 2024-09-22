0157adbee6825faebafc31712dc1c634b3c5d749
Elizigi <elizigi876@gmail.com>
2024-09-22 22:19:21 +0300

    Finished

--- 04 - Typescript/16- Agile - 20-09-2024/eli/src/render/render.ts
+++ 04 - Typescript/16- Agile - 20-09-2024/eli/src/render/render.ts
@@ -1,18 +1,21 @@
+import { Joke } from "../model/model";
+import { getButtons } from "../controllers/controllers";
 
-import {Joke} from "../model/model";
-
-
-
-const containerElement =document.querySelector<HTMLDivElement>('#app');
-
-export function render(jokes:Joke[])
-{
-    jokes.forEach(joke=>
-    {
-        const jokeElement=document.createElement("div")
-        jokeElement.classList.add("joker")
-        jokeElement.innerHTML=`${joke.leadUp} ${joke.punchline}`
-        containerElement?.appendChild(jokeElement);
-    }
-    )
+//const containerElement = document.querySelector<HTMLDivElement>("#app") as HTMLElement;
+const jokecontainerElement = document.getElementById("jokeList") as HTMLElement;
+export function render(jokes: Joke[]) {
+    jokecontainerElement.innerHTML="";
+  jokes.forEach((joke) => {
+    const jokeElement = document.createElement("div");
+    jokeElement.classList.add("joker");
+    jokeElement.id = joke.id;
+    jokeElement.innerHTML = `<button id=${joke.id}"edit">edit</button><h3 id=${joke.id}"leadUp">${joke.leadUp}</h3><br><h3 id=${joke.id}"punchline" > ${joke.punchline}</h3> <button id=${joke.id}"delete">delete</button>`;
+    jokeElement.style.margin ="3vh"
+     jokeElement.style.backgroundColor ="yellow";
+     jokeElement.style.width="80vh"
+     jokecontainerElement.appendChild(jokeElement);
+    getButtons(jokeElement, joke);
+  });
 }
+
+
