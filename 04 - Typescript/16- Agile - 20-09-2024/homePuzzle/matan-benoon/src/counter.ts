ed89b0bc91d2a3809b98d30eac91c14917bba34e
Matan Benoon <139704968+matanbn1121@users.noreply.github.com>
2024-09-25 19:08:24 +0300

    .

--- 04 - Typescript/16- Agile - 20-09-2024/homePuzzle/matan-benoon/src/counter.ts
+++ 04 - Typescript/16- Agile - 20-09-2024/homePuzzle/matan-benoon/src/counter.ts
@@ -2,6 +2,18 @@ import { joke } from "./models/model";
 
 export function renderJokes(element: HTMLElement, jokes: joke[]) {
   element.innerHTML = '';
-  jokes.forEach(joke => element.innerHTML += `<li> ${joke.id} ${joke.text}</li>
+  jokes.forEach(joke => element.innerHTML += `<li> ${joke.id} ${joke.text} <button onclick="clearResults()">remove</button></li>
     `);
 }
+
+
+export function displayResults(name: string): void {
+  const result = document.getElementById('result') as HTMLDivElement;
+  result.innerHTML = `<h2>yours jokes:</h2>
+ <p>the joke: ${name}</p> 
+ `;
+}
+
+export function clearResults(): void {
+  const resultDiv = document.getElementById('result') as HTMLDivElement;
+  resultDiv.innerHTML = '';}
--- 04 - Typescript/16- Agile - 20-09-2024/homePuzzle/matan-benoon/src/main.ts
+++ 04 - Typescript/16- Agile - 20-09-2024/homePuzzle/matan-benoon/src/main.ts
@@ -1,5 +1,6 @@
 import './style.css';
 import { renderJokes } from './counter';
+import { displayResults } from './counter';
 import { jokes } from './models/model';
 
 const jokesListElement = document.getElementById('joke-list');
@@ -7,3 +8,26 @@ const jokesListElement = document.getElementById('joke-list');
 if (jokesListElement) {
   renderJokes(jokesListElement, jokes);
 }
+
+export function handleSubmit(event: Event): void {
+    event.preventDefault();
+
+    const name = (document.getElementById('your-joke') as HTMLInputElement).value;
+
+    const newJoke = {
+      id: jokes.length + 1,
+      text: name
+    };
+    jokes.push(newJoke);
+
+    if (jokesListElement) {
+      renderJokes(jokesListElement, jokes);
+    }
+
+    displayResults(name);
+
+    (document.getElementById('your-joke') as HTMLInputElement).value = '';
+}
+
+const form = document.getElementById('myForm') as HTMLFormElement;
+form.addEventListener('submit', handleSubmit);
