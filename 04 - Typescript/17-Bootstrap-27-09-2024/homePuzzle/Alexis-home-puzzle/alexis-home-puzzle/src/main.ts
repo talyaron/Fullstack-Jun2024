fe5030f5d34e713685cb8dc544327aca1b688c01
alexis <gog20@icloud.com>
2024-10-01 06:51:09 +0300

    av hp

--- 04 - Typescript/17-Bootstrap-27-09-2024/homePuzzle/Alexis-home-puzzle/alexis-home-puzzle/src/main.ts
+++ 04 - Typescript/17-Bootstrap-27-09-2024/homePuzzle/Alexis-home-puzzle/alexis-home-puzzle/src/main.ts
@@ -1,23 +1,33 @@
 import './view/style.css'
-import typescriptLogo from './typescript.svg'
+import me from '/me.jpeg'
 import skaiipicture from '/skaii.jpg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
+    <a href="https://en.wikipedia.org/wiki/British_Shorthair" target="_blank">
       <img src="${skaiipicture}" class="logo" alt="Skaii" />
     </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+    <a href="https://www.instagram.com/p/CckxvqxoTuIqbJ5cTDnSfnIz9DYK6WP98SKnKE0/?utm_source=ig_web_button_share_sheet&igsh=MzRlODBiNWFlZA==" target="_blank">
+      <img src="${me}" class="logo vanilla" alt="Alexis" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>My life</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
+      Click here to learn more
     </p>
+    <button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
   </div>
 `
 
