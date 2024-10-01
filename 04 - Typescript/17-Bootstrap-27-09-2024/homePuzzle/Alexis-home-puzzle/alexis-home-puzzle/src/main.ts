1e4a74e52cff8af9931d720f0d27aaabdf7cba5a
alexis <gog20@icloud.com>
2024-10-01 06:43:03 +0300

    av hp

--- 04 - Typescript/17-Bootstrap-27-09-2024/homePuzzle/Alexis-home-puzzle/alexis-home-puzzle/src/main.ts
+++ 04 - Typescript/17-Bootstrap-27-09-2024/homePuzzle/Alexis-home-puzzle/alexis-home-puzzle/src/main.ts
@@ -1,12 +1,12 @@
-import './view/style.scss'
+import './view/style.css'
 import typescriptLogo from './typescript.svg'
-import viteLogo from '../public/vite.svg'
+import skaiipicture from '/skaii.jpg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+      <img src="${skaiipicture}" class="logo" alt="Skaii" />
     </a>
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
@@ -18,7 +18,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
     </p>
-
   </div>
 `
 
