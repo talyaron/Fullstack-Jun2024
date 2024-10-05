0cc94abb4d4d182f0866f4a317075dbfecd7370b
alexis <gog20@icloud.com>
2024-10-05 18:16:45 +0300

    av

--- 04 - Typescript/17-Bootstrap-27-09-2024/homePuzzle/exp/src/main.ts
+++ 04 - Typescript/17-Bootstrap-27-09-2024/homePuzzle/exp/src/main.ts
@@ -1,15 +1,15 @@
-import './view/style.scss'
+import './view/dist/style.css'
 import me from '/images/me.jpg'
 import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <a href="https://vitejs.dev" target="_blank">
+    <a href="https://www.instagram.com/p/Cin4IeYoDH5AeIk6z1y8zNoMTAXUIRQioYMXYs0/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
       <img src="${me}" class="logo" alt="image" />
     </a>
   
     <h1>Alexis Vishnevezky</h1>
-  
+
 `
 
 setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
