be3e8d0427b090e4b17504b934b9f5936936c11b
Ranazidan1 <halabyrana8@gmail.com>
2024-10-08 14:10:44 +0300

    Update main.ts

--- 04 - Typescript/18-recapTasks/Rana zidan/src/main.ts
+++ 04 - Typescript/18-recapTasks/Rana zidan/src/main.ts
@@ -7,7 +7,7 @@ class FormValidator {
     
     this.emailPattern = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
     
-    this.phonePattern = /^\+?\d{10,15}$/;
+    this.phonePattern = /^\+?\d{6,10}$/;
     
     this.passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
   }
