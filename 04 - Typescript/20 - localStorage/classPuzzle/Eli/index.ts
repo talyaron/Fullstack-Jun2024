7099760ce8346c71f7fbaecb8a8e1a3a10e6df30
Elizigi <elizigi876@gmail.com>
2024-10-15 21:34:00 +0300

    P1+p2

--- 04 - Typescript/20 - localStorage/classPuzzle/Eli/index.ts
+++ 04 - Typescript/20 - localStorage/classPuzzle/Eli/index.ts
@@ -6,25 +6,70 @@
 save the user details in the local storage. after reloading the page, the user details should be the same as before,
  and should be rendered to the screen.
 */
+//p1
+const containerElement = document.getElementById("container") as HTMLElement;
+const localStorageCount = localStorage.getItem("count");
+let count: number = localStorageCount ? JSON.parse(localStorageCount) : 0;
 
-const containerElement = document.getElementById("container")as HTMLElement;
-const localStorageCount= localStorage.getItem('count');
-let count:number = localStorageCount ? JSON.parse(localStorageCount) : 0;
-
-function countUp(){
-    containerElement.innerHTML=
-    `<button id ="btn">${count}</button>
+function countUp() {
+  containerElement.innerHTML = `<button id ="btn">${count}</button>
     `;
-    const btn= document.getElementById(`btn`);
-    if(btn)
-    btn.addEventListener(`click`,(event)=>
-{
-   count ++;
-   localStorage.setItem(`count`,JSON.stringify(count));
-   countUp()
-}
-)
+
+  const btn = document.getElementById(`btn`);
+  if (btn) btn.addEventListener(`click`, handleClick);
 }
 
-countUp()
+function handleClick() {
+  {
+    count++;
+    localStorage.setItem(`count`, JSON.stringify(count));
+    countUp();
+  }
+}
 
+countUp();
+
+//p2
+interface UserDetails{
+    name:string;
+    mail:string;
+    img:string;
+}
+
+const localStorageDetail = localStorage.getItem("userDetail");
+let userDetail: UserDetails[] = localStorageDetail ? JSON.parse(localStorageDetail) : [];
+const container2Element = document.getElementById("container3") as HTMLElement;
+
+
+
+function getUserDetails(event)
+{ 
+    event.preventDefault();
+    const formData = new FormData(event.target as HTMLFormElement);
+    const nameInput = formData.get("name") as string;
+    const emailInput = formData.get("email") as string;
+    const imgInput = formData.get("img") as string;
+
+   const _name= nameInput;
+   const _mail =emailInput;
+   const _img =imgInput;
+   if(_name&&_mail&&_img)
+   {
+    const user = {
+        name: _name,
+        mail: _mail,
+        img: _img
+    }
+    userDetail.push(user);
+    localStorage.setItem(`userDetail`,JSON.stringify(userDetail));
+    renderUsers();
+   }
+}
+
+function renderUsers(){
+    const usersHTML = userDetail.map(element => {
+        return `${element.name}<br>${element.mail}<br><br><img src="${element.img}"><br>`;
+    }).join('-');
+    container2Element.innerHTML   = usersHTML; 
+}
+renderUsers();
