07b5693e37ca2ea0f727a6489f507c2bb5259847
Sagiv-D <adhcsvi1@gmail.com>
2024-10-24 19:59:13 +0300

    pages add

--- 04 - Typescript/21 - PersonalProject/SagivDahan/home/dist/home.js
+++ 04 - Typescript/21 - PersonalProject/SagivDahan/home/dist/home.js
@@ -1,30 +1,25 @@
 //model
-
 //view
-
 //render sidebar to dom
 function addSidebarToPage() {
-    const sidebar = createSidebar();
+    var sidebar = createSidebar();
     document.body.appendChild(sidebar);
 }
 addSidebarToPage();
-
 //controller
 //sidebar create function
-function createSidebar(): HTMLElement {
+function createSidebar() {
     //sidebar div
-    const sidebar = document.createElement('div');
+    var sidebar = document.createElement('div');
     //add sidebar div element to class "sidebar"
     sidebar.className = 'sidebar';
-
     //sidebar header
-    const title = document.createElement('h2');
+    var title = document.createElement('h2');
     title.textContent = 'Menu';
     sidebar.appendChild(title);
-    const menu = document.createElement('ul');
-
+    var menu = document.createElement('ul');
     //array of links and etc..
-    const items = [
+    var items = [
         { name: 'Dashboard', link: './home.html' },
         { name: 'Profile', link: 'Profile.html' },
         { name: 'Courses', link: 'Courses.html' },
@@ -32,18 +27,15 @@ function createSidebar(): HTMLElement {
         { name: 'Forum', link: 'Forum.html' },
         { name: 'Lessons', link: 'Lessons.html' }
     ];
-
-    items.forEach(item => {
-        const listItem = document.createElement('li');
-        const anchor = document.createElement('a');
+    items.forEach(function (item) {
+        var listItem = document.createElement('li');
+        var anchor = document.createElement('a');
         anchor.textContent = item.name;
         anchor.href = item.link;
         anchor.target = '_self';
-
         listItem.appendChild(anchor);
         menu.appendChild(listItem);
     });
     sidebar.appendChild(menu);
     return sidebar;
 }
-
