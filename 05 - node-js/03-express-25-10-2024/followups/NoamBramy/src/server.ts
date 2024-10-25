223bb74dc3e867f3509a348e7dac20adeece94f9
NoamBramy <lironfn12@gmail.com>
2024-10-25 11:36:51 +0300

    followups

---
+++ 05 - node-js/03-express-25-10-2024/followups/NoamBramy/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express()
+const port = process.env.PORT || 3000
+console.log("Hi im from typescript")
+
+app.get('/', (req, res) => {
+  console.log("hi someone join your website.")
+  res.send("Hi im working here")
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+});
