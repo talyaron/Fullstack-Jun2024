8188829685b1f5c2c485913441db5feb3530435d
Sagiv-D <adhcsvi1@gmail.com>
2024-10-25 18:49:05 +0300

    STARTED PROJECT

---
+++ 05 - node-js/03-express-25-10-2024/HomePuzzles/SagivDahan/src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+const app = express()
+const port = process.env.PORT || 3000
+
+console.log("Server On");
+
+
+app.use(express.static('public'))
+
+//get = a method of http
+//route '/' 
+
+//req = request
+//res = response
+
+//event handler of get method
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
