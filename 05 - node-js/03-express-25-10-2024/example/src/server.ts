9dfac7943b3928856b76d48d0c37745e4b9052c4
Tal Yaron <tal.yaron@gmail.com>
2024-10-25 12:12:15 +0300

    class puzzle

--- 05 - node-js/03-express-25-10-2024/example/src/server.ts
+++ 05 - node-js/03-express-25-10-2024/example/src/server.ts
@@ -8,6 +8,8 @@ const x: number = 5;
 console.log(x);
 
 
+app.use(express.static('public')) //middleware
+
 //get = a method of http
 //route '/' 
 
@@ -15,10 +17,20 @@ console.log(x);
 //res = response
 
 //event handler of get method
-app.get('/', (req, res) => {
-    console.log("hello world");
-    res.send('Hello World!!!')
+app.get('/main', (req, res) => {
+    console.log("<h1 style='color: blue'>hello world</h1>");
+    let x:string = '';
+    for (let i = 0; i < 10; i++) {
+        x += `${i} ,`;
+    }
+    res.send(`<h1 style='color: blue'>hello world</h1><h2>from express</h2><p>${x}</p>`)
 })
+
+app.get('/about', (req, res) => {
+    res.send("<h1 style='color: green'>About us</h1>")
+})
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
