e897043d0c2ff39a816344b313a9a84b164fe21b
Ranazidan1 <halabyrana8@gmail.com>
2024-10-29 13:19:45 +0200

    rana

--- 05 - node-js/03-express-25-10-2024/HomePuzzles/Rana/src/dist/server.js
+++ 05 - node-js/03-express-25-10-2024/HomePuzzles/Rana/src/dist/server.js
@@ -4,7 +4,11 @@ var express_1 = require("express");
 var path_1 = require("path");
 var app = express_1["default"]();
 var port = 3000;
-app.use(express_1["default"].static(path_1["default"].join(__dirname, 'public')));
+app.use(express_1["default"].static('public'));
+app.get('/05 - node-js/03-express-25-10-2024/HomePuzzles/Rana', function (req, res) {
+    console.log("Rana zidan!");
+    res.sendFile(path_1["default"].resolve('public', 'index.html'));
+});
 app.listen(port, function () {
-    console.log("Server is running on http://localhost:" + port);
+    console.log("Server listening on port" + port);
 });
