7ebafce4ca42e5ace31466631e5337b0c01792a7
Elizigi <elizigi876@gmail.com>
2024-10-29 20:08:31 +0200

    EZPZLEMONSQUEEZY

---
+++ 05 - node-js/04-express-async-await-29-10-2024/classPuzzle/Eli/src/server.ts
@@ -0,0 +1,66 @@
+import express from 'express';
+const app = express()
+const port = process.env.PORT || 3000
+
+console.log("Hi from typescript");
+
+
+
+app.use(express.static('public')) //middleware
+
+//get = a method of http
+//route '/' 
+
+//req = request
+//res = response
+
+//event handler of get method
+app.get('/main', (req:any, res:any) => {
+    console.log("<h1 style='color: blue'>hello world</h1>");
+ 
+    res.send(`<h1 style='color: blue'>hello world</h1><h2>from express</h2><p>${x}</p>`)
+})
+
+app.get('/about', (req, res) => {
+    res.send("<h1 style='color: green'>About us</h1>")
+})
+let x:string = '';
+for (let i = 0; i < 10; i++) {
+    x += `${i} ,`;
+}
+//route
+app.get('/api/get-hello', (req, res)=>{
+
+    try{
+        // setTimeout(() => {
+        res.send({message: "Hello from express"});
+        // }, 3000);
+    } catch(error){
+        console.error(error);
+    }
+})
+//route
+app.get('/api/rng', (req, res)=>{
+
+    try{
+        // setTimeout(() => {
+        res.send({message: `bye from express ${x}`});
+        // }, 3000);
+    } catch(error){
+        console.error(error);
+    }
+})
+app.get('/api/countUp', (req, res)=>{
+
+    try{
+        // setTimeout(() => {
+        res.send({message: `is your random number :)`});
+        // }, 3000);
+    } catch(error){
+        console.error(error);
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
