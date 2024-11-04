80772d6741ab26844f8324dc92963bd05743e0cb
Toharkenin <109432113+Toharkenin@users.noreply.github.com>
2024-11-04 22:30:48 +0200

    update

--- 05 - node-js/05-CRUD-29-10-2024/HomePuzzle2/Tohar/public/dist/index.js
+++ 05 - node-js/05-CRUD-29-10-2024/HomePuzzle2/Tohar/public/dist/index.js
@@ -48,227 +48,92 @@ var User = /** @class */ (function () {
 }());
 ;
 function renderLoginPage() {
-    var loginForm = "\n     <div class=\"container\">\n            <h1>Login</h1>\n            <form id=\"loginForm\">\n                <input type=\"email\" class=\"input\" id=\"email\" name=\"email\" required placeholder=\"Email\">\n                <input type=\"password\" class=\"input\" id=\"password\" name=\"password\" required placeholder=\"Password\">\n                <a href=\"#forgotPassword\" class=\"forgotPsw\">Forgot Password?</a>                \n                <button class=\"loginBtn\" id=\"loginButton\" type=\"submit\">Login</button>\n                <a class=\"signupBtn\" id=\"button\" onclick=\"navigataToSignup()\">SIGN UP</a>\n            </form>\n            \n        </div>\n    ";
+    var loginForm = "\n     <div class=\"container\">\n            <h1>Login</h1>\n            <form onsubmit=\"handleFormLogin(event)\">\n                <input type=\"email\" class=\"input\" id=\"email\" name=\"email\" required placeholder=\"Email\">\n                <input type=\"password\" class=\"input\" id=\"password\" name=\"password\" required placeholder=\"Password\">\n                <a href=\"#forgotPassword\" class=\"forgotPsw\">Forgot Password?</a>                \n                <button class=\"loginBtn\" id=\"loginButton\" type=\"submit\">Login</button>\n                <a class=\"signupBtn\" id=\"button\" onclick=\"navigataToSignup()\">SIGN UP</a>\n            </form>\n            \n        </div>\n    ";
     var loginPageElement = document.querySelector('#loginPage');
     if (!loginPageElement)
         throw new Error('Login page not found');
     loginPageElement.innerHTML = loginForm;
-    handleFormLogin();
 }
 ;
-function handleFormLogin() {
+function handleFormLogin(event) {
     return __awaiter(this, void 0, void 0, function () {
-        var form, response, data;
+        var form, email, password;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
-                    form = document.getElementById('loginForm');
-                    console.log('form', form);
-                    return [4 /*yield*/, fetch('http://localhost:3000/api/get-posts')];
+                    event.preventDefault();
+                    form = event.target;
+                    email = form.elements.namedItem('email').value;
+                    password = form.elements.namedItem('password').value;
+                    return [4 /*yield*/, userExists(email)];
                 case 1:
-                    response = _a.sent();
-                    return [4 /*yield*/, response.json()];
+                    if (!_a.sent()) return [3 /*break*/, 3];
+                    return [4 /*yield*/, passwordNotCorrect(password)];
                 case 2:
-                    data = _a.sent();
-                    if (form) {
-                        form.addEventListener('submit', function (event) {
-                            event.preventDefault();
-                            var formData = new FormData(form);
-                            var email = formData.get('email');
-                            var password = formData.get('password');
-                            console.log('users', data.users);
-                            //Validation with localStorage
-                            // if (localStorage.getItem('email') !== email) {
-                            //     alert('Email does not exist');
-                            // } else if (localStorage.getItem('password') !== password) {
-                            //     alert('Password not valid');
-                            // } else {
-                            // console.log(email + ', ' + password);
-                            //     const newUrl = '?loginBtn=loginBtn';
-                            //     window.location.href = newUrl; 
-                            // }
-                        });
+                    if (_a.sent()) {
+                        alert('Password not correct');
                     }
                     else {
-                        console.error('Login form not found in the DOM');
+                        window.location.href = "../postsPage/postsPage.html";
                     }
-                    return [2 /*return*/];
+                    return [3 /*break*/, 4];
+                case 3:
+                    alert('You are not registered, please sign up');
+                    _a.label = 4;
+                case 4: return [2 /*return*/];
             }
         });
     });
 }
 ;
 function navigataToSignup() {
-    console.log('in navigata to sign');
     window.location.href = "./signup/signup.html";
 }
-renderLoginPage();
-function renderMainPage() {
-    var html = "\n    <h1>New Post</h1>\n    <form onsubmit=\"handleCreatePost(event)\">\n        <input type=\"text\" name=\"caption\" placeholder=\"Write a caption...\" required>\n        <input type=\"url\" name=\"imageURL\" placeholder=\"Enter image URL\" required>\n        <button type=\"submit\">Post</button>\n    </form>\n    ";
-    // <input type="file" name="image" id="imageUpload" name="imageUpload" accept="image/*">
-    document.querySelector('#main').innerHTML = html;
-}
 ;
-renderMainPage();
-function handleCreatePost(event) {
+function userExists(email) {
     return __awaiter(this, void 0, void 0, function () {
-        var form, caption, imageURL, response, error_1;
+        var response, data, error_1;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
-                    event.preventDefault();
-                    form = event.target;
-                    caption = form.elements.namedItem('caption').value;
-                    imageURL = form.elements.namedItem('imageURL').value;
-                    _a.label = 1;
+                    _a.trys.push([0, 3, , 4]);
+                    return [4 /*yield*/, fetch("http://localhost:3000/api/user-exists?email=" + encodeURIComponent(email))];
                 case 1:
-                    _a.trys.push([1, 3, , 4]);
-                    console.log('Sending post:', { caption: caption, imageURL: imageURL });
-                    return [4 /*yield*/, fetch('http://localhost:3000/api/add-post', {
-                            method: 'POST',
-                            headers: { 'Content-Type': 'application/json' },
-                            body: JSON.stringify({ caption: caption, imageURL: imageURL })
-                        })];
-                case 2:
                     response = _a.sent();
-                    if (!response.ok)
-                        throw new Error('Failed to add post');
-                    console.log('Post added successfully!');
-                    form.reset();
-                    fetchPosts();
-                    return [3 /*break*/, 4];
+                    return [4 /*yield*/, response.json()];
+                case 2:
+                    data = _a.sent();
+                    return [2 /*return*/, data.exists];
                 case 3:
                     error_1 = _a.sent();
-                    console.error('Error sending post:', error_1);
-                    return [3 /*break*/, 4];
+                    console.error("Error checking user existence:", error_1);
+                    return [2 /*return*/, false];
                 case 4: return [2 /*return*/];
             }
         });
     });
 }
-function fetchPosts() {
+function passwordNotCorrect(password) {
     return __awaiter(this, void 0, void 0, function () {
         var response, data, error_2;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
                     _a.trys.push([0, 3, , 4]);
-                    return [4 /*yield*/, fetch('http://localhost:3000/api/get-posts')];
+                    return [4 /*yield*/, fetch("http://localhost:3000/api/get-user?password=" + encodeURIComponent(password))];
                 case 1:
                     response = _a.sent();
                     return [4 /*yield*/, response.json()];
                 case 2:
                     data = _a.sent();
-                    // const feedElement = document.getElementById("feed");
-                    // if (!feedElement) throw new Error("Feed element not found");
-                    renderPosts(data.posts);
-                    return [3 /*break*/, 4];
+                    return [2 /*return*/, data.passwordNotCorrect];
                 case 3:
                     error_2 = _a.sent();
-                    console.error("Error fetching posts:", error_2);
-                    return [3 /*break*/, 4];
+                    console.error("Error checking user existence:", error_2);
+                    return [2 /*return*/, false];
                 case 4: return [2 /*return*/];
             }
         });
     });
 }
-;
-function renderPosts(posts) {
-    var feedElement = document.getElementById('feed');
-    if (!feedElement)
-        throw new Error('Feed element not found');
-    var htmlPosts = posts.map(function (post) {
-        return renderPost(post);
-    }).filter(function (post) { return post !== null; }).join('');
-    feedElement.innerHTML = htmlPosts;
-}
-;
-function renderPost(post) {
-    try {
-        // changeFileToImage();
-        var html = "\n        <div class=\"post\">\n            <img src=\"" + post.imageURL + "\" alt=\"Image\" />\n            <h3 id=\"caption-" + post.id + "\">" + post.caption + "</h3>\n            <button onclick=\"handleEditCaption('" + post.id + "')\" >Edit</button>\n            <button onclick=\"handlDeletePost('" + post.id + "')\">Delete</button>\n            <button onclick=\"handlEditImage\">Change Image</button>\n            <p>" + post.caption + "</p>\n        </div>\n        ";
-        return html;
-    }
-    catch (error) {
-        console.error('Error:', error);
-    }
-}
-function handleEditCaption(id) {
-    try {
-        var captionElement_1 = document.getElementById("caption-" + id);
-        if (!captionElement_1)
-            throw new Error('Caption element not found');
-        captionElement_1.contentEditable = 'true';
-        captionElement_1.focus();
-        captionElement_1.addEventListener("blur", function (event) {
-            var caption = captionElement_1.innerText;
-            captionElement_1.contentEditable = 'false';
-            fetchEditedTitle(id, caption);
-        });
-    }
-    catch (error) {
-        console.error('Error:', error);
-    }
-}
-;
-function fetchEditedTitle(id, caption) {
-    return __awaiter(this, void 0, void 0, function () {
-        var response, data;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0: return [4 /*yield*/, fetch('http://localhost:3000/api/update-post', {
-                        method: 'PATCH',
-                        headers: { 'Content-Type': 'application/json' },
-                        body: JSON.stringify({ id: id, caption: caption })
-                    })];
-                case 1:
-                    response = _a.sent();
-                    if (!response.ok) {
-                        console.error("Failed to update title:", response.statusText);
-                        return [2 /*return*/];
-                    }
-                    return [4 /*yield*/, response.json()];
-                case 2:
-                    data = _a.sent();
-                    console.log(data, 'success');
-                    return [2 /*return*/];
-            }
-        });
-    });
-}
-;
-function handlDeletePost(id) {
-    return __awaiter(this, void 0, void 0, function () {
-        var response, data, error_3;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 3, , 4]);
-                    return [4 /*yield*/, fetch('http://localhost:3000/api/delete-post', {
-                            method: 'DELETE',
-                            headers: { 'Content-Type': 'application/json' },
-                            body: JSON.stringify({ id: id })
-                        })];
-                case 1:
-                    response = _a.sent();
-                    console.log('in delete');
-                    if (!response.ok) {
-                        console.error("Failed to update title:", response.statusText);
-                        return [2 /*return*/];
-                    }
-                    return [4 /*yield*/, response.json()];
-                case 2:
-                    data = _a.sent();
-                    console.log(data, 'success');
-                    fetchPosts();
-                    return [3 /*break*/, 4];
-                case 3:
-                    error_3 = _a.sent();
-                    console.error('Error:', error_3);
-                    return [3 /*break*/, 4];
-                case 4: return [2 /*return*/];
-            }
-        });
-    });
-}
-;
+renderLoginPage();
