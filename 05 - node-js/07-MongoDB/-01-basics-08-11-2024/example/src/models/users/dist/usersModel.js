5d479044f2579e4c5ada1179595cd5d1b8627952
Tal Yaron <tal.yaron@gmail.com>
2024-11-08 11:47:41 +0200

    update

--- 05 - node-js/07-MongoDB/-01-basics-08-11-2024/example/src/models/users/dist/usersModel.js
+++ 05 - node-js/07-MongoDB/-01-basics-08-11-2024/example/src/models/users/dist/usersModel.js
@@ -1,6 +1,7 @@
 "use strict";
 exports.__esModule = true;
-exports.users = void 0;
+exports.UserModel = exports.UserSchema = exports.users = void 0;
+var mongoose_1 = require("mongoose");
 var User = /** @class */ (function () {
     function User(name, email, password) {
         this.id = crypto.randomUUID();
@@ -12,3 +13,11 @@ var User = /** @class */ (function () {
 }());
 exports["default"] = User;
 exports.users = [];
+//data structure
+exports.UserSchema = new mongoose_1.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+// model => collection
+exports.UserModel = mongoose_1.model('User', exports.UserSchema); // collection name is 'users' by default
--- 05 - node-js/07-MongoDB/-01-basics-08-11-2024/example/src/models/users/usersModel.ts
+++ 05 - node-js/07-MongoDB/-01-basics-08-11-2024/example/src/models/users/usersModel.ts
@@ -1,10 +1,12 @@
+import { model, Schema } from "mongoose";
+
 export default class User {
     id: string;
     name: string;
     email: string;
     password: string;
 
-    constructor( name: string, email: string, password: string) {
+    constructor(name: string, email: string, password: string) {
         this.id = crypto.randomUUID();
         this.name = name;
         this.email = email;
@@ -13,3 +15,13 @@ export default class User {
 }
 
 export const users: User[] = [];
+
+//data structure
+export const UserSchema = new Schema({
+    name: String,   
+    email: String,
+    password: String
+})
+
+// model => collection
+export const UserModel = model('User', UserSchema); // collection name is 'users' by default
