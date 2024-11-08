4c5df9e2eb1a3883eac6ef96fe7ef91619cf1503
Sagiv Dahan <adhcsvi1@gmail.com>
2024-11-08 12:06:35 +0200

    UPDATE

--- 05 - node-js/07-MongoDB/-01-basics-08-11-2024/followup/SagivDahan/src/models/users/usersModel.ts
+++ 05 - node-js/07-MongoDB/-01-basics-08-11-2024/followup/SagivDahan/src/models/users/usersModel.ts
@@ -6,7 +6,7 @@ export default class User {
     email: string;
     password: string;
 
-    constructor( name: string, email: string, password: string) {
+    constructor(name: string, email: string, password: string) {
         this.id = crypto.randomUUID();
         this.name = name;
         this.email = email;
@@ -16,12 +16,12 @@ export default class User {
 
 export const users: User[] = [];
 
-// data model
+//data structure
 export const UserSchema = new Schema({
-    name: String,
+    name: String,   
     email: String,
     password: String
 })
 
-// model to collection 
-export const userModel = model("User" , UserSchema);
+// model => collection
+export const UserModel = model('User', UserSchema); // collection name is 'users' by default
