8a783f20574065b03d3792a86a154d58634504a8
Toharkenin <109432113+Toharkenin@users.noreply.github.com>
2024-11-08 11:31:22 +0200

    Update usersModel.js

--- 05 - node-js/07-MongoDB/-01-basics-08-11-2024/followup/Tohar/src/models/users/dist/usersModel.js
+++ 05 - node-js/07-MongoDB/-01-basics-08-11-2024/followup/Tohar/src/models/users/dist/usersModel.js
@@ -1,6 +1,7 @@
 "use strict";
 exports.__esModule = true;
-exports.users = void 0;
+exports.userModel = exports.userSchema = exports.users = void 0;
+var mongoose_1 = require("mongoose");
 var User = /** @class */ (function () {
     function User(name, email, password) {
         this.id = crypto.randomUUID();
@@ -12,3 +13,9 @@ var User = /** @class */ (function () {
 }());
 exports["default"] = User;
 exports.users = [];
+exports.userSchema = new mongoose_1.Schema({
+    name: String,
+    email: String,
+    password: String
+});
+exports.userModel = mongoose_1.model('User', exports.userSchema);
