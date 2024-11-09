96da01460dfe170955c2cef178ca84fefc9435cd
Ranazidan1 <halabyrana8@gmail.com>
2024-11-09 14:19:55 +0200

    rana-1

---
+++ 05 - node-js/06-Routes/homePuzzle/Rana/src/server.ts
@@ -0,0 +1,63 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require('cors');
+app.use(cors());
+
+const posts: Array<{ title: string, text: string, imageURL: string }> = [];
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+
+app.post('/api/add-post', (req: any, res: any) => {
+    const { title, text, imageURL } = req.body;
+    
+    if (!title || !text || !imageURL) {
+        return res.status(400).json({ error: "All fields are required" });
+    }
+
+    posts.push({ title, text, imageURL });
+    console.log("Post added successfully");
+    res.status(201).json({ message: "Post added successfully" });
+});
+
+
+app.get('/api/get-posts', (req: any, res: any) => {
+    res.json({ posts });
+});
+
+
+app.put('/api/update-post/:index', (req: any, res: any) => {
+    const index = parseInt(req.params.index);
+    const { title, text, imageURL } = req.body;
+    
+    if (isNaN(index) || index < 0 || index >= posts.length) {
+        return res.status(400).json({ error: "Invalid index" });
+    }
+
+    if (title) posts[index].title = title;
+    if (text) posts[index].text = text;
+    if (imageURL) posts[index].imageURL = imageURL;
+    console.log("Post updated successfully!")
+    res.status(200).json({ message: "Post updated successfully" });
+});
+
+
+app.delete('/api/delete-post/:index', (req: any, res: any) => {
+    const index = parseInt(req.params.index);
+    
+    if (isNaN(index) || index < 0 || index >= posts.length) {
+        return res.status(400).json({ error: "Invalid index" });
+    }
+
+    posts.splice(index, 1);
+    console.log("deleted successfully");
+    res.status(200).json({ message: "Post deleted successfully" });
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
