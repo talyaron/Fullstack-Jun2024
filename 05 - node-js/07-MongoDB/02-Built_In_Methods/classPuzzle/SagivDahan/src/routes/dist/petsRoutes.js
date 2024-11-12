5fc1e1b37cd6f80a2fb69f4360e90511a3b84609
Sagiv Dahan <adhcsvi1@gmail.com>
2024-11-12 21:10:00 +0200

    class puzzle

---
+++ 05 - node-js/07-MongoDB/02-Built_In_Methods/classPuzzle/SagivDahan/src/routes/dist/petsRoutes.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var setPetsCont_1 = require("../controllers/pets/setPetsCont");
+var getPetsCont_1 = require("../controllers/pets/getPetsCont");
+var deletePetCont_1 = require("../controllers/pets/deletePetCont");
+var router = express_1["default"].Router();
+router.post('/add-pet', setPetsCont_1.addPet);
+router.get('/get-pets', getPetsCont_1.getPets);
+router["delete"]('delete-pet', deletePetCont_1.deletePet);
+exports["default"] = router;
