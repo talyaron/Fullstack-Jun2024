699a8ccb0782c19517ce20a945b75e89b99cf80e
Alexis <gog20@icloud.com>
2024-11-15 13:24:06 +0200

    av

---
+++ 05 - node-js/07-MongoDB/03-repeat-15-11-202/HomePuzzle/Alexis/public/index.ts
@@ -0,0 +1,33 @@
+
+
+export async function handleAddClient(ev:any) {
+    try {
+ev.preventDefault();
+const formData = new FormData(ev.target);
+const firstName = formData.get("firstName");
+const lastName = formData.get("lastName");
+const email = formData.get("email");
+const phone = formData.get("phone");
+const date = formData.get("date") as string;
+const yearOfBirth = new Date(date).getFullYear();
+
+
+const response = await fetch("/api/clients/add-client",{
+    method:'POST',
+    headers: {'Contenet-Type': 'application/json'},
+    body:JSON.stringify({
+        firstName,lastName,email,phone,yearOfBirth
+    })
+})
+
+if(response.ok){
+    const data = await response.json();
+    console.log(data);
+}
+
+        
+    } catch (error) {
+        console.error('error');
+    }
+    
+}
