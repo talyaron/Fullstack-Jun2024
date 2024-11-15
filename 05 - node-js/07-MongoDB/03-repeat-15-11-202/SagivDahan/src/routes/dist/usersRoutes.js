17ea734e07244954618dbcc0bd658a54564f2cfe
Sagiv Dahan <adhcsvi1@gmail.com>
2024-11-15 12:42:41 +0200

    Update

---
+++ 05 - node-js/07-MongoDB/03-repeat-15-11-202/SagivDahan/src/routes/dist/usersRoutes.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var router = express_1["default"].Router();
+// router.post("/register", register);
+// router.get("/login", login);
+// router.get("/getuser, getUser");
+exports["default"] = router;
---
+++ 05 - node-js/07-MongoDB/03-repeat-15-11-202/SagivDahan/src/routes/usersRoutes.ts
@@ -0,0 +1,8 @@
+import express from 'express';
+const router = express.Router()
+
+// router.post("/register", register);
+// router.get("/login", login);
+// router.get("/getuser, getUser");
+
+export default router;
