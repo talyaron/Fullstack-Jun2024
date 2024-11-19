d96d05c0bb4c431a79d3f20e0a84bf205a886891
Toharkenin <109432113+Toharkenin@users.noreply.github.com>
2024-11-19 18:54:03 +0200

    a

---
+++ 05 - node-js/07-MongoDB/03-repeat-15-11-202/HomePuzzle/Tohar/public/dashboard/dashboard.ts
@@ -0,0 +1,10 @@
+function renderDashboardPage() {
+    const userDetails = `
+        <div class="container">
+            <h3>ntyhrthgrht</h3>
+        </div>
+    `
+document.querySelector<HTMLDivElement>('#dashboardPage')!.innerHTML = userDetails;
+};
+
+renderDashboardPage();
---
+++ 05 - node-js/07-MongoDB/03-repeat-15-11-202/HomePuzzle/Tohar/public/dashboard/dist/dashboard.js
@@ -0,0 +1,6 @@
+function renderDashboardPage() {
+    var userDetails = "\n        <div class=\"container\">\n            <h3>ntyhrthgrht</h3>\n        </div>\n    ";
+    document.querySelector('#dashboardPage').innerHTML = userDetails;
+}
+;
+renderDashboardPage();
