a98655e6939e1803fd33b97450cc59b8b5363ac1
Yosef Ibrahim <yosefib88@gmail.com>
2024-12-24 20:30:47 +0200

    class puzle done

--- 06-React/01-basic/followup/yosef/yosef/src/App.tsx
+++ 06-React/01-basic/followup/yosef/yosef/src/App.tsx
@@ -2,34 +2,51 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import yoskPic from './assets/pics/aaa.png'
+import { Box, Box2 } from './view/components/box/Box'
+import { CSSProperties } from 'react';
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const display: CSSProperties = {
+    display: 'flex',
+    gap: '20px',
+    justifyContent: 'center',
+    alignItems: 'flex-start', 
+  };
+
+  const itemStyle: CSSProperties = {
+    textAlign: 'center',
+    maxWidth: '300px',
+  };
+
+  const imageStyle: CSSProperties = {
+    width: '100%', // התאמת התמונה לגודל ההורה
+    height: 'auto',
+    borderRadius: '8px', // עיגול פינות אופציונלי
+  };
+
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <p>Yosef Fovrite</p>
+      <div style={display}> 
+        <div style={itemStyle}>
+           <img alt="yosef pic" src={yoskPic} style={imageStyle}/>
+           <Box text="hasidjsaijdijadiasjdijsijasdey" />
+        </div>
+        <div style={itemStyle}>
+           <img alt="yosef pic" src={yoskPic} style={imageStyle}/>
+           <Box text="haoijasdijasiasjijdijasijaijsaijasidasjey" />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 10)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <h1>Yosef Forvrite</h1>
+      <div style={display}> 
+      <Box2 image='https://media.contentapi.ea.com/content/dam/ea/fifa/fifa-mobile/season-5/global_assets/images/2023/03/fifa-mobile-grid-tile-season-5-16x9-1.jpg.adapt.crop191x100.1200w.jpg' title='new new' text='hey' />
+      <Box2 image='https://media.contentapi.ea.com/content/dam/ea/fifa/fifa-mobile/season-5/global_assets/images/2023/03/fifa-mobile-grid-tile-season-5-16x9-1.jpg.adapt.crop191x100.1200w.jpg' title='new new' text='hey' />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
