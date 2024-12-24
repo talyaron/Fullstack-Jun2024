f02f603d18e0fca341fc85e6a1983689986a2504
Sagiv Dahan <adhcsvi1@gmail.com>
2024-12-24 19:56:27 +0200

    Update App.tsx

--- 06-React/01-basic/classPuzzle/SagivDahan/sagivDahan/src/App.tsx
+++ 06-React/01-basic/classPuzzle/SagivDahan/sagivDahan/src/App.tsx
@@ -2,8 +2,7 @@ import './App.css'
 import Post from './view/componnents/Post'
 
 function App() {
-
-
+  
   return (
     <>
       <Post headTitle="Hello!" imageUrl="https://static.vecteezy.com/vite/assets/photo-masthead-375-BoK_p8LG.webp" postDesctiption="Lorem ipuosom"></Post>
