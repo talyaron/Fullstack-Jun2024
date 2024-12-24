8f532031a17bfe76fa62c8758f91852c9d421b0e
Elizigi <elizigi876@gmail.com>
2024-12-24 18:37:03 +0200

    Done

--- 06-React/01-basic/followup/Eli/src/App.tsx
+++ 06-React/01-basic/followup/Eli/src/App.tsx
@@ -1,19 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Box from './view/compenents/Box/Box';
 function calculator()
 {
 console.log(1+2);
 }
 function App() {
-  const [count, setCount] = useState(0)
+  const [] = useState(0)
 
   return (
     <div>
-   <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque eligendi nulla esse soluta aliquid! Enim repudiandae minus quos eum odit, voluptate vel illo dolor? Deserunt doloremque iste laudantium velit similique. </h1>
-   <button id='button' onClick={calculator}>1+2</button>
+<img  src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_dark_color_272x92dp.png" alt="google" />  
+ <button id='button' onClick={calculator}>1+2</button>
+    <Box />
+
+    <Box />
     </div>
   )
 }
