dc412790e83ef3a044464dafb940e2ebf95af1a6
Elizigi <elizigi876@gmail.com>
2024-12-24 18:19:26 +0200

    Calc

--- 06-React/01-basic/followup/Eli/src/App.tsx
+++ 06-React/01-basic/followup/Eli/src/App.tsx
@@ -3,12 +3,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+function calculator()
+{
+console.log(1+2);
+}
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
    <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque eligendi nulla esse soluta aliquid! Enim repudiandae minus quos eum odit, voluptate vel illo dolor? Deserunt doloremque iste laudantium velit similique. </h1>
+   <button id='button' onClick={calculator}>1+2</button>
     </div>
   )
 }
