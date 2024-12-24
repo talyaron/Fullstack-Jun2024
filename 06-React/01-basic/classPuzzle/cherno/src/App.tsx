50cceb08ab85c320d7a73e961fea3df3a80aaacc
Cherno <daniel.cherno9@gmail.com>
2024-12-24 20:30:17 +0200

    added classpuzzle

---
+++ 06-React/01-basic/classPuzzle/cherno/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+import Articles from './view/components/article/Articles'
+
+function App() {
+  return (
+    <>
+      <Articles/>
+    </>
+  )
+}
+
+export default App
