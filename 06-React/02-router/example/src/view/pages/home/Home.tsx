2ee3be9bf699fd2aac1dca5ce0f43c6fd9c67d7a
Tal <tal.yaron@gmail.com>
2024-12-27 09:51:39 +0200

    router dom

---
+++ 06-React/02-router/example/src/view/pages/home/Home.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+
+const Home = () => {
+  return (
+    <div>
+        <h1>Home</h1>
+        <Link to="/main">Goto Main</Link>
+
+    </div>
+  )
+}
+
+export default Home
---
+++ 06-React/02-router/example/src/view/pages/main/Main.tsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+
+const Main = () => {
+  return (
+    <div>
+        <h1>Main</h1>
+        <Link to="/">Goto Home</Link>
+    
+    </div>
+  )
+}
+
+export default Main
