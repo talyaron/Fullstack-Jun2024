270a4cfb89b96af5f8c51031a47b07b1177b16ac
Matan Benoon <139704968+matanbn1121@users.noreply.github.com>
2024-12-31 15:28:52 +0200

    follow up matan router

---
+++ 06-React/02-router/followups/matan/src/view/components/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+      <Link to="/Store">Store</Link>
+    </div>
+  )
+}
+
+export default Home
