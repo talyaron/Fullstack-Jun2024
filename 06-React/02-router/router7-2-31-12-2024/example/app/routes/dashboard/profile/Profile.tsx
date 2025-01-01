741a82546ccd13492e670d9d8be69a539f9eca6e
Tal Yaron <tal.yaron@gmail.com>
2025-01-01 20:05:23 +0200

    cont

---
+++ 06-React/02-router/router7-2-31-12-2024/example/app/routes/dashboard/profile/Profile.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+      <div style={{ backgroundColor: 'teal', flex:"auto"
+       }}>Profile</div>
+  )
+}
+
+export default Profile
