4de42b6d0e3ce4075aaa951f83f79042f7e519b1
Annapetr0 <45635544+AnnaPetr0@users.noreply.github.com>
2025-03-02 10:46:50 +0200

    home puzzle

---
+++ 05 - node-js/07-MongoDB/03-repeat-15-11-202/HomePuzzle/anna/src/dist/server.js
@@ -0,0 +1,24 @@
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var mongoose_1 = require("mongoose");
+var app = express_1["default"]();
+var port = process.env.PORT || 3000;
+app.use(express_1["default"].json()); // takes the header of the request and if it is json it will parse it into an object and attach it to the request object (req.body)
+//header -> req.body
+app.use(express_1["default"].static('public'));
+//DB
+var dbUrl = "mongodb+srv://annapetrovitsky:CUA5rKNGRSDqqUOE@course.l5x0p.mongodb.net";
+var database = 'fs-jun24';
+//connection
+mongoose_1["default"].connect(dbUrl + "/" + database).then(function () {
+    console.info("DB connected");
+})["catch"](function (err) {
+    console.error(err);
+});
+//routesD
+var usersRoutes_1 = require("./routes/usersRoutes");
+app.use("/api/users", usersRoutes_1["default"]);
+app.listen(port, function () {
+    console.log("Server listening on port " + port);
+});
---
+++ 05 - node-js/07-MongoDB/03-repeat-15-11-202/HomePuzzle/anna/src/server.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import mongoose from 'mongoose';
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+
+
+app.use(express.json()); // takes the header of the request and if it is json it will parse it into an object and attach it to the request object (req.body)
+//header -> req.body
+
+app.use(express.static('public'));
+
+//DB
+const dbUrl = "mongodb+srv://annapetrovitsky:CUA5rKNGRSDqqUOE@course.l5x0p.mongodb.net"
+const database = 'fs-jun24';
+
+//connection
+mongoose.connect(`${dbUrl}/${database}`).then(()=>{
+    console.info("DB connected")
+}).catch((err)=>{
+    console.error(err)
+});
+
+//routesD
+import usersRoutes from './routes/usersRoutes';
+app.use("/api/users", usersRoutes);
+
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
